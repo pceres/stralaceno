e984171e18323d6775489ba5aad8e0167091d1dd
ceres <ceres>
2005-02-07 15:25:52 +0000

    Gestione dell'indentazione nel codice generato, al fine di ottenere un codice html leggibile.

--- header.php
+++ header.php
@@ -1,18 +1,27 @@
-<table>
-  <tbody>
-    <tr width="20%">
-      <td width="20%"> <img src="images/sweb.gif" alt="logo_stralacenoweb"> </td>
-      <!--td>
-      <embed src="images/filmatoflash300x70.swf"
- quality="high" alt="logo_stralacenoweb_flash"
- pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=3DShockwaveFlash"
- type="application/x-shockwave-flash"> </td-->
-    </tr>
-  </tbody>
-</table>
+<!-- 
+inizio header
+-->
+	<table>
+	  <tbody>
+	  
+		<tr width="20%">
+		  <td width="20%"> <img src="images/sweb.gif" alt="logo_stralacenoweb"> </td>
+		  <!--td>
+			  <embed
+				 src="images/filmatoflash300x70.swf" quality="high" alt="logo_stralacenoweb_flash"
+				 pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=3DShockwaveFlash"
+				 type="application/x-shockwave-flash">
+		    </td-->
+		</tr>
+		
+	  </tbody>
+	</table>
 
-<hr>
-<span style="font-size: 13px; color: rgb(30, 60, 90);">La Stralaceno &egrave; una manifestazione podistica 
+	<hr>
+	<span style="font-size: 13px; color: rgb(30, 60, 90);">La Stralaceno &egrave; una manifestazione podistica 
 amatoriale su strada, riservata ai caposelesi, che si tiene ogni anno a fine agosto/inizio settembre sul circuito
 di Laceno.</span>
-<hr>
+	<hr>
+<!-- 
+fine header
+-->
--- index_right.php
+++ index_right.php
@@ -2,27 +2,29 @@
 
 $colore_bordo_right = '#FFFFFF';
 
-$open_border = "\n<tr><td><table width=\"95%\" border=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"$colore_bordo_right\"><tbody><tr><td>\n<table width=\"100%\" border=\"0\" cellspacing=\"\" cellpadding=\"5\" bgcolor=\"$colore_sfondo\"><tbody><tr><td>\n";
-$close_border = "</td></tr></tbody></table>\n</td></tr></tbody></table></td></tr>\n\n";
+$open_border = "\t<tr><td><table width=\"95%\" border=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"$colore_bordo_right\"><tbody><tr><td>\n\t   <table width=\"100%\" border=\"0\" cellspacing=\"\" cellpadding=\"5\" bgcolor=\"$colore_sfondo\"><tbody><tr><td>\n";
+$close_border = "\t   </td></tr></tbody></table>\n\t</td></tr></tbody></table></td></tr>\n\n";
 
 ?>
-
+<!-- 
+inizio colonna destra
+-->
 <table width="95%" border="0" cellspacing="0" align="center" bgcolor="#336699"><tbody><tr><td>
-	<table width="100%" border="0" cellspacing="" cellpadding="5" bgcolor="#ffffff"><tbody>
-	
+   <table width="100%" border="0" cellspacing="" cellpadding="5" bgcolor="#ffffff"><tbody>
 
 
-		<?php echo $open_border?>
+<?php echo $open_border?>
+
+
 		<strong>Approfondimenti:</strong>
 
-			<br>›&nbsp;<a href="filtro6.php" name="Archivio storico per tempi">Archivio storico (tutti i risultati ordinati per tempi)</a>
-			<br>›&nbsp;<a href="filtro8.php" name="grafico tempi">Grafico andamento tempi negli anni</a>
+		<br>&#8250&nbsp;<a href="filtro6.php" name="Archivio storico per tempi">Archivio storico (tutti i risultati ordinati per tempi)</a>
+		<br>&#8250&nbsp;<a href="filtro8.php" name="grafico tempi">Grafico andamento tempi negli anni</a>
 
-
-			<form action="filtro2.php" method="POST">
-			›&nbsp;Archivio storico personale:
+		<form action="filtro2.php" method="POST">
+		&#8250&nbsp;Archivio storico personale:
 			<select name="nome">
-			<?php
+<?php
 			$elenco_nomi = array();
 			$elenco_cognomi = array();
 			$elenco_i = array();
@@ -47,20 +49,17 @@ $close_border = "</td></tr></tbody></table>\n</td></tr></tbody></table></td></tr
 			array_multisort($elenco_cognomi,SORT_ASC, SORT_STRING,$elenco_nomi,SORT_ASC, SORT_STRING, $elenco_i,SORT_ASC, SORT_NUMERIC);
 	
 			for ($i = 0; $i < count($elenco_nomi); $i++) {
-				echo "<option value=\"".$elenco_i[$i]."\">".$elenco_nomi[$i]."</option>\n";
+				echo "\t\t\t<option value=\"".$elenco_i[$i]."\">".$elenco_nomi[$i]."</option>\n";
 				}
-	
-			?>	
-			</select>
-			<input type="submit" value="Mostra prestazioni personali">
-			</form>
+?>	
+		</select>
+		<input type="submit" value="Mostra prestazioni personali"></form>
 
 
-
-			<form action="filtro4.php" method="POST">
-			›&nbsp;Archivio storico annuale (tutti i risultati di un anno):
-			<select name="anno">
-			<?php
+		<form action="filtro4.php" method="POST">
+		&#8250&nbsp;Archivio storico annuale (tutti i risultati di un anno):
+		<select name="anno">
+<?php
 			$elenco_anni = array();	# elenco degli anni in archivio
 			$elenco_i = array();		# elenco delle i in corrispondenza delle quali trovare gli anni
 			for ($i = 1; $i < count($archivio); $i++) {
@@ -75,20 +74,23 @@ $close_border = "</td></tr></tbody></table>\n</td></tr></tbody></table></td></tr
 			array_multisort($elenco_anni,SORT_DESC, SORT_NUMERIC, $elenco_i,SORT_ASC, SORT_NUMERIC);
 	
 			for ($i = 0; $i < count($elenco_anni); $i++) {
-				echo "<option value=\"".$elenco_anni[$i]."\">".$elenco_anni[$i]."</option>\n";
+				echo "\t\t\t<option value=\"".$elenco_anni[$i]."\">".$elenco_anni[$i]."</option>\n";
 				}
-			?>	
-			</select>
-			<input type="submit" value="Mostra prestazioni dell'anno">
-			</form>
-
-			›&nbsp;<a name="personaggi">I personaggi</a></li>
-			<br>›&nbsp;<a name="organizzatori">Gli organizzatori</a></li>
-			<br>›&nbsp;<a name="classifica partecipazioni">Classifica partecipazioni</a></li>
-
-		<?php echo $close_border?>
+?>	
+		</select>
+		<input type="submit" value="Mostra prestazioni dell'anno">
+		</form>
 
 
+		&#8250&nbsp;<a name="personaggi">I personaggi</a>
+		<br>&#8250&nbsp;<a name="organizzatori">Gli organizzatori</a>
+		<br>&#8250&nbsp;<a name="classifica partecipazioni">Classifica partecipazioni</a>
 
-	</td></tr></tbody></table>
+
+<?php echo $close_border?>
+
+   </td></tr></tbody></table>
 </td></tr></tbody></table>
+<!-- 
+fine colonna destra
+-->
