981e3a877cd7a295bf2fb1f7a55dac74787b762f
ceres <ceres>
2005-02-22 13:23:07 +0000

    Aggiunta articolo di Angelo Ceres

--- index_middle.php
+++ index_middle.php
@@ -1,18 +1,48 @@
 <?php
 	#  $colore_bordo e $colore_sfondo devono essere definiti esternamente!!!
 	$colore_bordo_middle = '#ffffff';#$colore_bordo;
-	$colore_sfondo_middle = '#FFF6F0';#"#e4eeff";#$colore_sfondo;
+	$colore_sfondo_middle = '#FFF6F0';#$colore_sfondo;
 	
-	$open_border = "\t<tr><td><table width=\"95%\" border=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"$colore_bordo_middle\"><tbody><tr><td>\n\t   <table width=\"100%\" border=\"0\" cellspacing=\"\" cellpadding=\"5\" bgcolor=\"$colore_sfondo_middle\"><tbody><tr><td>\n";
+	$open_border = "\t<tr><td><table width=\"99%\" border=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"$colore_bordo_middle\"><tbody><tr><td>\n\t   <table width=\"100%\" border=\"0\" cellspacing=\"\" cellpadding=\"5\" bgcolor=\"$colore_sfondo_middle\"><tbody><tr><td>\n";
 	$close_border = "\t   </td></tr></tbody></table>\n\t</td></tr></tbody></table></td></tr>\n\n";
 
 ?>
 <!-- 
 inizio colonna centrale
 -->
-<table width="95%" border="0" cellspacing="0" align="center" bgcolor="#336699"><tbody><tr><td>
+<table width="100%" border="0" cellspacing="0" align="center" bgcolor="#336699"><tbody><tr><td>
    <table width="100%" border="0" cellspacing="" cellpadding="5" bgcolor="#ffffff"><tbody>
 	
+<?php echo $open_border;?>
+		<h3>L'altopiano del Laceno</h3>
+		<div align="justify" style="font-family:Comic sans ms, Arial, Verdana, Helvetica, sans-serif;font-size:0.78em">
+			<p>Laceno è ritenuto uno degli altopiani più ameni dell'appennino meridionale. 
+Chi arriva da Caposele, per la strada interna montana, affacciandosi sull'altopiano subisce
+un piacevole impatto con la bellezza del luogo.
+Laceno è posto a 1050 metri di altitudine con una superfice di circa  10 chilometri quadrati;
+la prateria è dominata dal maestoso Cervialto che con i suoi 1809 m. è la seconda vetta più alta 
+della Campania (la prima dei monti Picentini), in passato era frequentato dai cervi, da cui
+il suo nome, ed era anche rifugio dei briganti.
+Più ad ovest si erge il monte Rajamagra solcato da piste da sci e da impianti di risalita che 
+arrivano fino a quota 1667 metri.
+Dalla cima il panorama è stupendo: da una parte si spazia dalla penisola sorrentina con Capri,
+fino a Paestum e Punta Licosa; dall'altra colline e paesi d'Irpinia fin verso il tavoliere 
+delle Puglie ad intravedere il mar Adriatico.
+			<p>E' coperto da alberi di alto fusto di faggio e di pino; normalmente da novembre a marzo è
+ammantato di neve. In pieno inverno si possono toccare temperature minime intorno ai meno
+venti gradi centigradi e d'estate difficilmente e raramente si superano i 28 gradi.
+Fino ad un paio di secoli fa vi abbondava il cervo, oggi è ancora presente il lupo, la volpe,
+la faina, la martora, la lepre, il cinghiale. Fra i volatili l'aquila reale, i falchi, corvi,
+starne, il tordo, il colombaccio.
+			<p>Particolare è il rapporto che lega Laceno a Caposele, infatti, grazie alle abbondanti 
+precipitazioni, con una media annua di 2000 mm., inizia il lungo viaggio delle acque verso
+le sorgenti di piazza Sanità che fanno di Caposele il paese d'Italia più ricco d'acqua;
+a sottolineare questo connubio è anche il nome che prende una parte dell'altopiano,
+precisamente la parte nord, come Foce di Caposele.
+			<div align="right" style="padding-right:2em;"><i>Angelo Ceres</i></div>
+		</div>
+		
+<?php echo $close_border;?>
 	
 <?php echo $open_border;?>
 		<h3>Presentazione</h3>
