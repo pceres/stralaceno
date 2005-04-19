3579c38ac62c679c66917927855e4e7c158c79de
ceres <ceres>
2005-04-19 07:51:13 +0000

    backport dal ramo online: - aggiunta foglio di stile css - contatore

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,14 @@
 #!/usr/local/bin/php 
-<!DOCTYPE public "-//w3c//dtd html 4.01 transitional//en" 
-		"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 TRANSITIONAL//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
   <title>Amministrazione</title>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   <meta name="GENERATOR" content="Quanta Plus">
+  <style type="text/css">@import "/work/stralaceno2/css/stralaceno.css";</style>
 </head>
 
-<body>
+<body class="admin">
 
 <form enctype="multipart/form-data" action="upload.php" method="post">
 <input type="hidden" name="MAX_FILE_SIZE" value="30000">
@@ -38,6 +38,12 @@ Password: <input name="password" type="password">
 
 <a href='articoli.php'>Gestione articoli</a>
 
+<?php
+include('../libreria.php');
+
+# logga il contatto
+$counter = count_page("admin_index",array("COUNT"=>1,"LOG"=>1),$filedir_counter); # abilita il contatore, senza visualizzare le cifre, e fai il log
+?>
 
 </body>
 </html>
