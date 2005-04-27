f06a0070db8d095c3736eb364799d99da608e801
ceres <ceres>
2005-04-27 12:20:05 +0000

    Gestisce le operazioni di gestione degli articoli online: modifica numero ed ordine degli articoli online, cancellazione, upload (non ancora)

---
+++ admin/manage_articles.php
@@ -0,0 +1,58 @@
+#!/usr/local/bin/php 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 TRANSITIONAL//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>Gestione articoli in prima pagina</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <meta name="GENERATOR" content="Programmers Notepad">
+  <style type="text/css">@import "/work/stralaceno2/css/stralaceno.css";</style>
+</head>
+<body>
+
+<?php
+
+require_once('../libreria.php');
+
+$mode = $_REQUEST['task'];
+$data = $_REQUEST['data'];
+
+switch ($mode)
+{
+case 'set_online_articles':
+	$article_list = split('::',$data); // elenco dei titoli da pubblicare
+	
+	set_online_articles($article_online_file,$article_list);
+	
+	echo "Fatto!<br>\n";
+	echo "<br>\n";
+	echo "Gli articoli online sono:<br>\n";
+	echo "<ul>\n";
+	for ($i = 0; $i < count($article_list); $i++) 
+	{
+		$art_data = load_article($article_list[$i]);
+		
+		echo "<li>id ".$article_list[$i].") ". $art_data['titolo'] ."</li>\n";
+	} 
+	echo "</ul><br>\n";
+	
+	
+
+	break;
+case 'delete':
+	echo "delete";
+	break;
+default:
+	die("Compito non specificato!");
+}
+
+
+log_action($articles_dir,"Action: $mode, data<$data>");
+
+?>
+
+<hr>
+<a href="articoli.php">Torna indietro</a>
+
+</body>
+</html>
+ 
