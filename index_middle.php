72d2bc6fd4d8545f08dc848ac5c25b94169abd7e
ceres <ceres>
2005-04-27 12:16:47 +0000

    Gestione del caso particolare in cui non e' presente nessun articolo in prima pagina

--- index_middle.php
+++ index_middle.php
@@ -7,15 +7,25 @@ inizio colonna centrale
 
 	$art_list = get_online_articles($article_online_file); // carica l'elenco degli articoli da pubblicare
 
-	for ($i = 0; $i < count($art_list); $i++)
+	if (count($art_list) > 0)
 	{
-		$art_data = load_article($art_list[$i]); // carica l'articolo
-		
-		if (!empty($art_data)) // se l'articolo esiste...
+		for ($i = 0; $i < count($art_list); $i++)
 		{
-			show_article($art_data);	// visualizza l'articolo
-		}
-	}	
+			$art_data = load_article($art_list[$i]); // carica l'articolo
+			
+			if (!empty($art_data)) // se l'articolo esiste...
+			{
+				show_article($art_data);	// visualizza l'articolo
+			}
+		}	
+	}
+	else
+	{
+		$art_data["titolo"]="Nessun articolo!";
+		$art_data["autore"]="";
+		$art_data["testo"]=array("Per pubblicare un articolo, se sei amministratore vai alla pagina admin/index.php");
+		show_article($art_data);	// visualizza l'articolo
+	}
 
 	// incrementa il contatore per la homepage
 	$counter = count_page("homepage",array("COUNT"=>1,"LOG"=>1),$filedir_counter); # abilita il contatore, senza visualizzare le cifre, e fai il log
