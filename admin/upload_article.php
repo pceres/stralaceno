bec38a90894eda47d394caba347a2c5a54535a13
ceres <ceres>
2005-04-27 12:22:07 +0000

    - link per tornare alla pagina amministrativa; - la stringa di errore termina lo script (die() invece di echo); - log_data per lasciare traccia delle operazioni effettuate; - publish_articles sdoppiata in upload_article() e publish_online_articles();

--- admin/upload_article.php
+++ admin/upload_article.php
@@ -2,7 +2,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 TRANSITIONAL//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-  <title>Archivio Stralaceno</title>
+  <title>Caricamento articoli sul sito</title>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   <meta name="GENERATOR" content="Quanta Plus">
   <style type="text/css">@import "/work/stralaceno2/css/stralaceno.css";</style>
@@ -12,7 +12,7 @@
 
 <?php
 
-include '../libreria.php';
+require_once('../libreria.php');
 $uploaddir = $articles_dir;
 
 $password_ok = "stralacenoadmin"; 
@@ -42,10 +42,8 @@ if ($ok == TRUE)
 {
 	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploaddir . $new_name)) 
 	{ 
-		$file = fopen($uploaddir . 'something_changed.txt', "a");
-		fputs($file, $uploaddir . $new_name . ", " . date("l dS of F Y h:i:s A") . " \n");
-		fclose($file);
-
+		log_action($uploaddir,$uploaddir . $new_name . ", " . date("l dS of F Y h:i:s A"));
+		
 		print "Il file $new_name &egrave; stato inviato con successo.\n"; 
 	}
 	else
@@ -58,16 +56,16 @@ if ($ok == TRUE)
 		{
 	  		case UPLOAD_ERR_INI_SIZE :
 		  	case UPLOAD_ERR_FORM_SIZE :
-				print "File troppo grande!\n"; 
+				die("File troppo grande!\n"); 
 				break;
 			case UPLOAD_ERR_PARTIAL :
-				print "Upload eseguito parzialmente!\n"; 
+				die("Upload eseguito parzialmente!\n"); 
 				break;
 			case UPLOAD_ERR_NO_FILE :
-				print "Nessun file &egrave; stato inviato!\n"; 
+				die("Nessun file &egrave; stato inviato!\n"); 
 				break;
 			default:
-				echo "Unknown error type: [$errno]<br>\n";
+				die("Unknown error type: [$errno]<br>\n");
 				break;
 	  	}
 	 }
@@ -83,14 +81,27 @@ echo "title: $title\n";
 echo "autore: $author\n";
 
 $bulk = file($uploaddir . $new_name);
+unlink($uploaddir . $new_name); // cancella il file in maniera che l'articolo non venga ancora considerato pubblicato
 
-publish_article($id_articolo,$author,$title,$bulk,$uploaddir.$new_name,$max_online_articles);
+// salva sul sito l'articolo (ma non e' ancora online)
+$id_articolo = upload_article($author,$title,$bulk,$uploaddir);
+
+// leggi l'elenco degli articoli gia' online
+$art_list = get_online_articles($article_online_file); // carica l'elenco degli articoli da pubblicare
+
+// aggiungi il nuovo articolo in cima
+$art_list = array_merge($id_articolo,$art_list);
+
+// salva il nuovo elenco degli articoli online
+publish_online_articles($art_list);
 
 
 # logga il contatto
 $counter = count_page("admin_upload_articoli",array("COUNT"=>1,"LOG"=>1),$filedir_counter); # abilita il contatore, senza visualizzare le cifre, e fai il log
 ?>
 
+<hr>
+<a href="articoli.php">Torna indietro</a>
 
 </body>
 </html>
