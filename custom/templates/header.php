560cfdd2514470c7f9f329a0af576a868179598a
ceres <ceres>
2005-05-09 13:06:46 +0000

    spostati dalla radice

---
+++ custom/templates/header.php
@@ -0,0 +1,23 @@
+<!-- 
+inizio header
+-->
+	<table align="center">
+	  <tbody>
+	  
+		<tr>
+		  <td> <img src="/work/stralaceno2/custom/images/logo.gif" alt="logo_stralacenoweb"> </td>
+		  <!--td> <img src="<?php echo $root_path?>custom/images/logo.gif" alt="logo_stralacenoweb"> </td-->
+		</tr>
+
+	  </tbody>
+	</table>
+
+	<hr>
+	<span class="txt_normal">
+	La Stralaceno &egrave; una manifestazione podistica 
+amatoriale su strada, riservata ai caposelesi, che si tiene ogni anno a fine agosto/inizio settembre sul circuito
+di Laceno.</span>
+	<hr>
+<!-- 
+fine header
+-->
