d9c0b02115544607e5ae3605ffd97d29726e80cc
ceres <ceres>
2005-05-26 11:17:57 +0000

    - link disabilitati con a fianco icona di 'lavori in corso';

--- index_right.php
+++ index_right.php
@@ -134,18 +134,18 @@ selezioni di qualsiasi campo select all'interno del documento.
 			</td></tr>
 			
 			<tr style="vertical-align: baseline"><td>&#8250;&nbsp;</td><td>
-				<a class="disabled" onClick="alert('Pagina in allestimento!')" name="grafico_tempi" class="txt_link">Grafico andamento tempi negli anni</a>
-				<!--img src="< ?php echo $site_abs_path?>images/work-in-progress.gif" alt="work in progress" width="25"-->
+				<a class="disabled" onClick="alert('Pagina in allestimento!')" name="grafico_tempi">Grafico andamento tempi negli anni</a>
+				<img src="<?php echo $site_abs_path?>images/work-in-progress.png" width="25" alt="work in progress">
 			</td></tr>
 			
 			<tr style="vertical-align: baseline"><td>&#8250;&nbsp;</td><td>
-				<a class="disabled" onClick="alert('Pagina in allestimento!')" name="classifica_partecipazioni" class="txt_link">Classifica partecipazioni</a>
-				<!--img src="< ?php echo $site_abs_path?>images/work-in-progress.gif" alt="work in progress" width="25"-->
+				<a class="disabled" onClick="alert('Pagina in allestimento!')" name="classifica_partecipazioni">Classifica partecipazioni</a>
+				<img src="<?php echo $site_abs_path?>images/work-in-progress.png" width="25" alt="work in progress">
 			</td></tr>
 			
 			<tr style="vertical-align: baseline"><td>&#8250;&nbsp;</td><td>
-				<a class="disabled" onClick="alert('Pagina in allestimento!')" name="personaggi" class="txt_link">I personaggi</a>
-				<!--img src="< ?php echo $site_abs_path?>images/work-in-progress.gif" alt="work in progress" width="25"-->
+				<a class="disabled" onClick="alert('Pagina in allestimento!')" name="personaggi">I personaggi</a>
+				<img src="<?php echo $site_abs_path?>images/work-in-progress.png" width="25" alt="work in progress">
 			</td></tr>
 			
 		  </tbody>
