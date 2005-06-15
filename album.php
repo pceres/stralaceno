88ab152e5675b34e860f60e862a172daa76de970
ceres <ceres>
2005-06-15 12:02:30 +0000

    backport dal ramo principale: - implementata la visualizzazione su piu' righe;

--- album.php
+++ album.php
@@ -26,7 +26,7 @@ $id_descrizione_foto = 2;
 
 $album = $elenco_foto[$anno];
 
-$photo_per_row = 5; // numero di foto per riga
+$photo_per_row = 3; // numero di foto per riga
 
 ?>
 
@@ -38,7 +38,7 @@ $photo_per_row = 5; // numero di foto per riga
 			
 			<!-- riga vuota -->
 			<tr>
-				<td colspan="<?php echo $photo_per_row ?>" height="10"><img src="Binder_files/Null.gif" border="0" height="10" width="11"></td>
+				<td colspan="<?php echo $photo_per_row ?>" height="10"><img src="<?php echo $site_abs_path ?>custom/images/cornice/Null.gif" border="0" height="10" width="11"></td>
 			</tr>
 			
 			
@@ -46,7 +46,8 @@ $photo_per_row = 5; // numero di foto per riga
 			<tr>
 				<td colspan="<?php echo $photo_per_row-1; ?>">
 					<font color="#000000" face="Times New Roman,Georgia,Times" size="4">
-					&nbsp;&nbsp;&nbsp;&nbsp;<a href="album.php">Foto disponibili per l'edizione <?php echo $anno ?></a>:
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						Foto disponibili per l'<a href="filtro4.php?anno=<?php echo $anno ?>">edizione <?php echo $anno ?></a>:
 					</font></td>
 					
 				<td valign="top" width="<?php echo round(100/$photo_per_row) ?>%">
@@ -57,11 +58,17 @@ $photo_per_row = 5; // numero di foto per riga
 			</tr>
 			
 			
+<?php 
+$photo_count = 0;
+$rem_count = 0;
+for ($riga=0; $photo_count<count($album); $riga++)
+{
+?>
+			
 			<!-- riga di $photo_per_row foto -->
 			<tr>
 <?php
 //print_r($album);
-$photo_count = 0;
 for ($i = 0; $i<$photo_per_row; $i++)
 {
 	if ($photo_count < count($album))
@@ -74,7 +81,7 @@ for ($i = 0; $i<$photo_per_row; $i++)
 				<td colspan='3'><img src="<?php echo $site_abs_path ?>custom/images/cornice/Bord_TL.gif" width="30" height="16" border="0" /><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_T.gif' width='105' height='16' border='0' /><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_TR.gif' width='30' height='16' border='0' /></td>
 			</tr> <tr>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_LT.gif' width='16' height='13' border='0' /></td>
-				<td rowspan='3' valign=middle><a href='<?php echo $nome_foto ?>'><img src='<?php echo $nome_foto ?>' border='0' width='133' height='100' /></a></td>
+				<td rowspan='3' valign=middle><a href='show_photo.php?id_photo=<?php echo $photo_count ?>&album=<?php echo $anno ?>'><img src='<?php echo $nome_foto ?>' border='0' width='133' height='100' /></a></td>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_RT.gif' width='16' height='13' border='0' /></td>
 			</tr> <tr>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_L.gif' width='16' height='74' border='0' /></td>
@@ -98,7 +105,6 @@ for ($i = 0; $i<$photo_per_row; $i++)
 			<!-- riga di $photo_per_row commenti -->
 			<tr>
 <?php
-$rem_count = 0;
 for ($i = 0; $i<$photo_per_row; $i++)
 {
 	if ($rem_count < count($album))
@@ -106,7 +112,7 @@ for ($i = 0; $i<$photo_per_row; $i++)
 		$nome_foto = $site_abs_path."custom/album/$anno/".$album[$rem_count][$id_nomefile_foto];
 ?>
 			<td align="center" valign="top" width="<?php echo round(100/$photo_per_row); ?>%">
-					<a href="<?php echo $nome_foto ?>">
+					<a href='show_photo.php?id_photo=<?php echo $rem_count ?>&album=<?php echo $anno ?>'>
 					<?php echo $album[$rem_count][$id_titolo_foto]?></a>
 					
 						<br>
@@ -123,6 +129,14 @@ for ($i = 0; $i<$photo_per_row; $i++)
 ?>				
 				
 </tr>
+
+<?php 
+} // for ($riga=0; $riga<2; $riga++)
+?>
+
+
+
+
 </tbody></table>
 
 </body>
