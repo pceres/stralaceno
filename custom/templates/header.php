168d43fe16d6f7b3f7440a38ad4973109b43525a
ceres <ceres>
2005-07-04 16:08:33 +0000

    backport dal ramo principale: - il link all'animazione flash e' di classe header_link;

--- custom/templates/header.php
+++ custom/templates/header.php
@@ -13,9 +13,13 @@ inizio header
 
 	<hr>
 	<span class="txt_normal">
-	<a class="txt_link" href="<?php echo $site_abs_path?>custom/moduli/presentazione/present.swf">La Stralaceno &egrave; una manifestazione podistica 
+	<!--a class="txt_link" href="<?php echo $site_abs_path?>custom/moduli/presentazione/present.swf">La Stralaceno &egrave; una manifestazione podistica 
 amatoriale su strada, riservata ai caposelesi, che si tiene ogni anno a fine agosto/inizio settembre sul circuito
-di Laceno.</a></span>
+di Laceno.</a-->
+	<a class="header_link" href="<?php echo $site_abs_path?>custom/moduli/presentazione/present.swf">La Stralaceno &egrave; una manifestazione podistica 
+amatoriale su strada, riservata ai caposelesi, che si tiene ogni anno a fine agosto/inizio settembre sul circuito
+di Laceno.</a>
+	</span>
 	<hr>
 <!-- 
 fine header
