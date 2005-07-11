8020bca7810ada0090ff7c8cd11a65ae0190e5c9
ceres <ceres>
2005-07-11 07:42:06 +0000

    - i thumbs vengono inpaginati a fine caricamento con script Javascript; questo permette la presenza di thumbs verticali; - vengono formattati correttamente gli spazi nei nomi; - vengono usati gli stili invece che la proprieta background: ora viene superato il test w3c;

--- album.php
+++ album.php
@@ -31,7 +31,64 @@ $photo_per_row = 3; // numero di foto per riga
 
 ?>
 
-<body>
+<body onLoad="FixPhotos('thumb_table')">
+
+<script type="text/javascript">
+//<![CDATA[
+<!--
+
+function FixPhotos(tag) {
+
+var table;
+var i,tag_i,thumb_id;
+
+i=0;
+do
+{
+	tag_i = tag+"_"+i;
+	thumb_id = "img_"+i;
+	
+	if (table = document.getElementById(tag_i))
+	{
+		//alert(tag_i);
+		FixSinglePhoto(table,thumb_id);
+	}
+	
+	i++;
+	
+} while (table);
+
+} // end FixPhoto
+
+
+function FixSinglePhoto(table,thumb_id) {
+
+var cell_L,cell_R,image;
+var	x;
+
+if (document.all)
+{ // IE
+	cell_L=table.cells[4];
+	cell_R=table.cells[5];
+}
+else if (document.getElementById)
+{ // NS
+	cell_L=table.tBodies[0].rows[2].cells[0];
+	cell_R=table.tBodies[0].rows[2].cells[1];
+}
+
+image=document.getElementById(thumb_id);
+x = image.height; // altezza della foto
+	
+cell_L.height=x-13*2;
+cell_R.height=x-13*2;
+
+} // end FixSinglePhoto
+
+//-->
+//]]>
+</script>
+
 
 	<!--body alink="#603913" background="Binder_files/bkgrnd.gif" link="#603913" text="#000000" vlink="#603913"-->
 		<table border="0" cellpadding="0" cellspacing="0" width="100%">
@@ -78,7 +135,6 @@ for ($i = 0; $i<$photo_per_row; $i++)
 		$nome_foto = $album[$photo_count][$id_nomefile_foto];
 		$pos = strrpos($nome_foto,'.');
 		$nome_thumb = substr($nome_foto,0,$pos)."-thumb".substr($nome_foto,$pos);
-		//$nome_thumb = str_replace(' ', '%20', $nome_thumb);
 		if (file_exists($root_path."custom/album/$anno/".$nome_thumb))
 		{	// esiste il thumbnail, usalo
 			$nome_foto = $site_abs_path."custom/album/$anno/".$nome_thumb;
@@ -87,19 +143,20 @@ for ($i = 0; $i<$photo_per_row; $i++)
 		{	// usa direttamente la foto
 			$nome_foto = $site_abs_path."custom/album/$anno/".$nome_foto;
 		}
+		$nome_foto = str_replace(' ', '%20', $nome_foto); // formatta correttamente gli spazi in html
 		
 ?>
 <td align='center' width='<?php echo round(100/$photo_per_row); ?>%' valign='middle'>
-		<table border='0' cellpadding='0' cellspacing='0'>
+		<table id="thumb_table_<?php echo $photo_count ?>" border='0' cellpadding='0' cellspacing='0'>
 			<tr>
 				<td colspan='3'><img src="<?php echo $site_abs_path ?>custom/images/cornice/Bord_TL.gif" alt="TL" width="30" height="16" border="0" /><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_T.gif' alt="T" width='105' height='16' border='0' /><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_TR.gif' alt="TR" width='30' height='16' border='0' /></td>
 			</tr> <tr>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_LT.gif' alt="LT" width='16' height='13' border='0' /></td>
-				<td rowspan='3' valign=middle><a href='show_photo.php?id_photo=<?php echo $photo_count ?>&amp;album=<?php echo $anno ?>'><img src='<?php echo $nome_foto ?>' border='0' width='133' height='100' /></a></td>
+				<td rowspan='3' valign=middle><a href='show_photo.php?id_photo=<?php echo $photo_count ?>&amp;album=<?php echo $anno ?>'><img id="img_<?php echo $photo_count ?>" src='<?php echo $nome_foto ?>' border='0' width='133' /></a></td>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_RT.gif' alt="RT" width='16' height='13' border='0' /></td>
 			</tr> <tr>
-				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_L.gif' alt="L" width='16' height='74' border='0' /></td>
-				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_R.gif' alt="R" width='16' height='74' border='0' /></td>
+				<td style="background-image:url(<?php echo $site_abs_path ?>custom/images/cornice/Bord_L.gif);">&nbsp;</td>
+				<td style="background-image:url(<?php echo $site_abs_path ?>custom/images/cornice/Bord_R.gif);">&nbsp;</td>
 			</tr> <tr>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_LB.gif' alt="LB" width='16' height='13' border='0' /></td>
 				<td><img src='<?php echo $site_abs_path ?>custom/images/cornice/Bord_RB.gif' alt="RB" width='16' height='13' border='0' /></td>
