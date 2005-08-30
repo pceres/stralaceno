8a89e785c51babb7c9ae1779dc45838c25e604ed
ceres <ceres>
2005-08-30 12:28:59 +0000

    - nuovo file .css;

--- custom/moduli/presentazione/presentazione.php
+++ custom/moduli/presentazione/presentazione.php
@@ -12,7 +12,7 @@ extract(indici());
   <title>Stralaceno Web - Presentazione della Stralaceno</title>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   <meta name="GENERATOR" content="Quanta Plus">
-  <style type="text/css">@import "<?php echo $css_site_path ?>/stralaceno.css";</style>  
+  <style type="text/css">@import "<?php echo $filename_css ?>";</style>  
 </head>
 <body>
 
