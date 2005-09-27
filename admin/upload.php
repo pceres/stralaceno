245f4a6bfc49986e9021e6a27d906024eb209316
ceres <ceres>
2005-09-27 08:02:41 +0000

    Backport dal ramo online: - flag che permette la scrittura 0002 invece di 0200;

--- admin/upload.php
+++ admin/upload.php
@@ -36,7 +36,7 @@ if ($ok == TRUE)
 {
 	// verifica che il file non sia protetto in scrittura
 	$perm = substr(sprintf('%o', fileperms($uploaddir . $new_name)), -4); // attributi in forma ottale
-	$write_enable = '0200'; // bit di attributi in forma ottaleche consentono la scrittura
+	$write_enable = '0002'; // bit di attributi in forma ottaleche consentono la scrittura
 	if (file_exists($uploaddir . $new_name) && ((octdec($perm) & octdec($write_enable)) == 0) )
 	{
 		die("Il file e' protetto in scrittura (".$perm.")! Contatta il webmaster.");
