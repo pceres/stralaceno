de8b61e9ae0bf089c86f42a56489104cb2ec3cb2
ceres <ceres>
2005-11-02 10:10:27 +0000

    - aggiunta la creazione e cancellazione (se e' vuoto) di un album fotografico;

--- admin/manage_album.php
+++ admin/manage_album.php
@@ -31,6 +31,9 @@ $mode = $_REQUEST['task'];
 $data = $_REQUEST['data'];
 $password = $_REQUEST['password'];
 
+log_action($album_dir,"Author:<".$_SERVER['REMOTE_ADDR'].">, Action: <$mode>, data: <$data>, ".date("l dS of F Y h:i:s A"));
+
+
 $password_ok = $password_album;
 
 if ($password != $password_ok)
@@ -44,10 +47,30 @@ switch ($mode)
 case 'cancel':
 	echo("Cancellazione della foto ".$data.":<br><br>\n");
 
+	// verifica che il file esiste
 	if (!is_file($data))
 	{
 		echo "Il file $data non esiste!<br><br>\n";
 		die("<a href=\"$referer\">Torna indietro</a><br>");
+	}
+	
+	// verifica che la foto non sia visibile
+	$elenco_foto = get_config_file($filename_albums,3); // carica il file di configurazione degli album
+	preg_match_all('/[^\/]+$/',dirname($data),$album);
+	$album = $album[0][0];
+	$foto = basename($data);
+	if (array_key_exists($album,$elenco_foto))
+	{
+		$album_data = $elenco_foto[$album];
+		foreach ($album_data as $foto_data)
+		{
+			if ($foto_data[0]==$foto) // la foto e' visibile, esci
+			{
+				echo "La foto $foto e' ancora visibile nell'album \"$album\"!<br>\n";
+				echo "Prima di cancellarla renderla invisibile modificando il file di configurazione degli album!<br><br>\n";
+				die("<a href=\"$referer\">Torna indietro</a><br>");
+			}
+		}
 	}
 	
 	// a questo punto sono sicuro che il file esiste
@@ -69,13 +92,96 @@ case 'cancel':
 
 	break;
 	
+case 'cancel_album':
+
+	$album = basename($data); // poiche' si accede direttamente al filesystem, elimina tutte le possibilita' di abusare  utilizzando nel path il '..'
+
+	echo("Cancellazione dell'album \"".$album."\":<br><br>\n");
+
+	if (!is_dir($album_dir.$album))
+	{
+		echo "La directory $album_dir"."$album non esiste!<br>\r";
+		echo "Per cancellare ogni traccia dell'album, eliminare, dal file di configurazione degli album fotografici,";
+		echo "la riga contenente \"[$data]\"<br><br>\n";
+		die("<a href=\"$referer\">Torna indietro</a><br>");
+	}
+	
+	// carica il file di configurazione degli album
+	$elenco_foto = get_config_file($filename_albums,3);
+	
+	if (count($elenco_foto[$album]) > 0)
+	{
+		echo "L'album \"$album\" non e' vuoto, procedere prima alla cancellazione di tutte le foto in esso presenti!<br>\r";
+		die("<a href=\"$referer\">Torna indietro</a><br>");
+	}
+	
+	// a questo punto sono sicuro che il file esiste
+	if (rmdir($album_dir.$album)) // cancello fisicamente la directory
+	{
+		echo "L'album \"$album\" e' stato cancellato!<br>\n";
+		echo "Per cancellare ogni traccia dell'album, eliminare, dal file di configurazione degli album fotografici,";
+		echo "la riga contenente \"[$album]\" e tutti i dati ad esso relativi<br><br>\n";
+	}
+	else
+	{
+		echo "Errore nella rimozione della directory \"$album\".";
+		echo "Verificare che siano state cancellate tutte le foto in essa contenute<br>\n";
+		die("<a href=\"$referer\">Torna indietro</a><br>");
+	}
+
+	break;
+
+case 'create_album':
+
+	$album = basename($data); // poiche' si accede direttamente al filesystem, elimina tutte le possibilita' di abusare  utilizzando nel path il '..'
+
+	echo("Creazione dell'album \"".$album."\":<br><br>\n");
+
+	// se la directory gia' esiste dai errore ed esci
+	if (is_dir($album_dir.$album))
+	{
+		echo "La directory $album_dir"."$album gia' esiste!<br>\r";
+		die("<a href=\"$referer\">Torna indietro</a><br>");
+	}
+	
+	// a questo punto sono sicuro che la directory puo' essere creata: procedi
+	if (mkdir($album_dir.$album)) // creo fisicamente la directory
+	{
+		echo "L'album $album e' stato creato!<br>\n";
+		echo "Procedere ad aggiungere foto all'album dalla pagina amministrativa<br>\n";
+	}
+	else
+	{
+		echo "Errore nella creazione della directory \"$album\".";
+		echo "Contattare l'amministratore<br>\n";
+		die("<a href=\"$referer\">Torna indietro</a><br>");
+	}
+
+	// aggiungi la riga [nome_album] nel file di configurazione
+	$tempfilename = $album_dir.'tempfile.txt';
+	if ($handle=fopen($tempfilename,'w'))
+	{
+		$bulk = file($filename_albums);
+		array_push($bulk,"\r\n","[$album]\r\n"); // aggiungi la riga col nome dell'album
+		foreach ($bulk as $line)
+		{
+			fwrite($handle,$line);
+		}
+		fclose($handle);
+		rename($tempfilename,$filename_albums);
+	}
+	else
+	{
+		echo "Non e' stato possibile modificare automaticamente il file di configurazione.";
+		echo "Procedere manualmente, aggiungendo una riga con \"[$album]\".";
+	}
+
+	break;
+	
 default:
 	echo "<a href=\"<?php echo $referer; ?>\">Torna indietro</a><br><br>\n";
 	die("mode: \"".$mode."\", data: \"".$data."\"\n");
 }
-
-
-log_action($album_dir,"Author:<".$_SERVER['REMOTE_ADDR'].">, Action: <$mode>, data: <$data>, ".date("l dS of F Y h:i:s A"));
 
 ?>
 
