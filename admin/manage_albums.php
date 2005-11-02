ce7887bbe7392a7ad57bcec44f72f18e574cb00f
ceres <ceres>
2005-11-02 12:21:52 +0000

    Backport dal ramo principale: - aggiunta creazione e cancellazione (se non ci sono piu' foto visibili o invisibili) di un album fotografico; - vengono visualizzati anche gli album non presenti in albums.txt;

--- admin/manage_albums.php
+++ admin/manage_albums.php
@@ -25,6 +25,51 @@ if ( !isset($_SERVER['HTTP_REFERER']) | ("http://".$_SERVER['HTTP_HOST'].$script
 <body class="admin">
 
 <script type="text/javaScript" src="<?php echo $site_abs_path ?>admin/md5.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+<!--
+
+function do_action(action,data)
+{
+	
+	switch (action)
+	{
+	case "new_album":
+		data = document.forms["form_new_album"].data.value;
+		
+		var msg="Confermi la creazione dell'album \""+data+"\"?";
+		if (!confirm(msg))
+		{
+			return false;
+		}
+		
+		document.forms["form_new_album"].password.value=hex_md5(document.forms["form_new_album"].password.value);
+		document.forms["form_new_album"].task.value='create_album';
+		document.forms["form_new_album"].submit();
+		
+		break;
+	case "cancel_album":
+		
+		var msg="Confermi la cancellazione dell'album \""+data+"\"?";
+		if (!confirm(msg))
+		{
+			return false;
+		}
+		
+		document.forms["form_new_album"].password.value=hex_md5(document.forms["form_new_album"].password.value);
+		document.forms["form_new_album"].data.value=data;
+		document.forms["form_new_album"].task.value='cancel_album';
+		document.forms["form_new_album"].submit();
+		
+		break;
+	}
+}
+
+//-->
+//]]>
+</script>
+
+<script type="text/javaScript" src="<?php echo $site_abs_path ?>admin/md5.js"></script>
 
 Albums attuali:<br>
 <small>formato:  file::titolo::descrizione::nomi_persone</small>
@@ -49,11 +94,38 @@ Visualizza tutte le foto disponibili per ciascun album:<br>
 <form name="debug_album" action="<?php echo $script_abs_path; ?>album.php" method="post">
 <?php
 $elenco_foto = get_config_file($filename_albums,3);
+
+// verifica se ci sono directory nascoste (cioe' esistono in $album_dir, e non c'e' la rispettina riga [nome_album] nel file di configurazione)
+if ($dh = opendir($album_dir)) 
+{
+	while (($file = readdir($dh)) !== false)
+	{
+	   if (is_dir($album_dir.$file) && !in_array(strtoupper($file),array('.','..','CVS','.CVS')))
+		{
+			if (!isset($elenco_foto[$file]))
+			{
+				$elenco_foto[$file] = array();
+			}
+		}
+	}
+	closedir($dh);
+}
+
 foreach ($elenco_foto as $anno => $album)
 {
-	echo "<a href=\"$script_abs_path"."album.php?anno=$anno&amp;password=show_all_photos\">$anno</a> ";
+	echo "<span style=\"white-space: nowrap;\"><a href=\"$script_abs_path"."album.php?anno=$anno&amp;password=show_all_photos\">$anno</a>\n ";
+	echo "<small>(<a onClick=\"do_action('cancel_album','$anno')\">Cancella</a>)&nbsp;&nbsp;</small></span>\n ";
 }
 ?>
+</form>
+
+<hr>
+
+<form name="form_new_album" action="<?php echo $script_abs_path; ?>admin/manage_album.php" method="post">
+	Crea un nuovo album: <input type="edit" name="data">
+	<input type="hidden" name="task">
+	Password: <input name="password" type="password">
+	<input type="submit" value="Invia File" onClick="do_action('new_album','')">
 </form>
 
 <?php
