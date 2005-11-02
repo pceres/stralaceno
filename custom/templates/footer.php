1044c1e26d3ea3ccb15aa44be183b5848aedadc9
ceres <ceres>
2005-11-02 10:19:46 +0000

    - aggiunto simbolo di validazione ok degli stili css a fianco della validazione w3c;

--- custom/templates/footer.php
+++ custom/templates/footer.php
@@ -34,12 +34,19 @@ inizio footer
 
 		<!-- la riga che segue e' il logo del validatore HTML W3C -->
 		<!--tr>
-		  <td colspan="3" style="width: 500px; vertical-align: bottom; height: 50px; text-align: center;">
-			  <a href="http://validator.w3.org/check?uri=referer"><img border="0"	src="http://www.w3.org/Icons/valid-html401" 
-				alt="Valid HTML 4.01!" height="31" width="88"></a>
+		  <td colspan="3" style="width: 500px; vertical-align: bottom; height: 50px; text-align: center;"-->
+			<!-- Validazione HTML -->
+			<!--a href="http://validator.w3.org/check?uri=referer">
+				<img border="0"	src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!" height="31" width="88">
+			</a-->
+			  
+			<!-- Validazione CSS -->
+			<!--a href="http://jigsaw.w3.org/css-validator">
+				<img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!">
+			</a>
 		  </td>
 		</tr-->
-
+		
 	  </tbody>
 	</table>
 
