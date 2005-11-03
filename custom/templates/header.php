b1fd416808b5320d66e87a00b79bf19bfeaed925
ceres <ceres>
2005-11-03 16:45:00 +0000

    - header homepage del sito di Nusco;

--- custom/templates/header.php
+++ custom/templates/header.php
@@ -5,12 +5,7 @@ inizio header
 	  <tbody>
 	  
 		<tr>
-		  <!--td> <img src="<?php echo $site_abs_path?>custom/images/logo.gif" alt="logo_stralacenoweb"> </td-->
-		  <td>
-			<object	type="application/x-shockwave-flash" data="<?php echo $site_abs_path ?>/custom/images/barrasuperiore.swf" width="456" height="105">
-				<param name="movie" value="<?php echo $site_abs_path ?>/custom/images/barrasuperiore.swf" />
-			</object>
-		  </td>
+		  <td> <img src="<?php echo $site_abs_path?>custom/images/logo.gif" alt="logo_maratonina_nusco"> </td>
 		</tr>
 		
 	  </tbody>
@@ -18,9 +13,9 @@ inizio header
 
 	<hr>
 	<span class="txt_normal">
-	<a class="header_link" href="<?php echo $site_abs_path?>custom/moduli/presentazione/present.swf">La Stralaceno &egrave; una manifestazione podistica 
-amatoriale su strada, riservata ai caposelesi, che si tiene ogni anno a fine agosto/inizio settembre sul circuito
-di Laceno.</a>
+	<!--a class="txt_link"-->La Maratonina di Nusco &egrave; una manifestazione podistica su
+su strada, su un tracciato di 10 km, che si tiene ogni anno ad agosto nel centro abitato di
+Nusco.<!--/a-->
 	</span>
 	<hr>
 <!-- 
