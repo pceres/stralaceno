588f2993a2094e6b58d3c43c426ed37ca5680b64
ceres <ceres>
2005-11-03 08:16:28 +0000

    - cliccando sull'icona della validazione css adesso viene automaticamente rivalida la pagina del sito;

--- custom/templates/footer.php
+++ custom/templates/footer.php
@@ -41,7 +41,7 @@ inizio footer
 			</a>
 			  
 			<!-- Validazione CSS -->
-			<a href="http://jigsaw.w3.org/css-validator">
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">
 				<img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!">
 			</a>
 		  </td>
