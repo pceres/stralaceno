7cc97898562815d8d94461fa51034abc87603c7d
ceres <ceres>
2005-11-16 12:52:23 +0000

    Backport dal ramo principale: - elenco utenti e relativi gruppi di appartenenza;

---
+++ custom/config/users.php
@@ -0,0 +1,13 @@
+#
+# elenco degli utenti e delle relative password
+#
+# il formato e' del tipo:
+# <user>::<userpass>::<group1>,...,<groupn>::
+#
+
+[users]
+ceres::f505a4f2dfe4f6071f265d170c34d2f7::admin,users
+alex::534b44a19bf18d20b71ecc4eb77c572f::admin,users
+nick::e2e42a07550863f8b67f5eb252581f6d::admin,users
+michele::8a94bdfc825df46f880854f41fee346b::users
+
