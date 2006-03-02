beb2bc691d0c70bce50d6ef38cb3330771afa0e1
ceres <ceres>
2006-03-02 20:50:54 +0000

    - visualizza le giocate della lotteria dei mondiali;

--- temp.php
+++ temp.php
@@ -1,16 +1,55 @@
-<!DOCTYPE public "-//w3c//dtd html 4.01 transitional//en" 
-		"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 TRANSITIONAL//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?php
+
+require_once('libreria.php');
+
+# dichiara variabili
+extract(indici());
+
+$id_questions = 1;
+
+$file_questions = $root_path."custom/lotterie/lotteria_".sprintf("%03d",$id_questions).".txt";	// nome del file di configurazione
+$file_log_questions = $root_path."custom/lotterie/lotteria_".sprintf("%03d",$id_questions)."_log.txt";	// nome del file di registrazione
+
+
+$lotteria = get_config_file($file_questions);
+$nome_lotteria = $lotteria["Attributi"][0][0];
+
+?>
 <html>
 <head>
-  <title>a</title>
+  <title>Archivio giocate <?php echo $nome_lotteria ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   <meta name="GENERATOR" content="Quanta Plus">
+  <style type="text/css">@import "<?php echo $filename_css ?>";</style>
 </head>
 <body>
   
 <?php
 
-print_r($_SERVER);
+
+$bulk = get_config_file($file_log_questions);
+
+echo "<div class=\"titolo_tabella\">Giocate &quot;$nome_lotteria&quot;</div>\n";
+
+echo '<table border=1><tbody>';
+
+$count_giocata = 0;
+foreach($bulk['default'] as $giocata)
+{
+	echo "<tr>";
+	echo "<td>".($count_giocata+1)."</td>\n";
+	echo "<td>".$giocata[0]."</td>\n";
+	//echo "<td>".$giocata[1]."</td>\n";
+	echo "<td>".$giocata[2]."</td>\n";
+	echo "<td>".$giocata[3]."</td>\n";
+	echo "</tr>";
+	$count_giocata++;
+}
+
+echo "</tbody></table>";
+
+die();
 
 /*
  locale:
