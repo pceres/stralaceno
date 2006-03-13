6969aa3ef1a0b9c52690aa21cd4690ad16521ccf
ceres <ceres>
2006-03-13 21:57:06 +0000

    - script Matlab per modificare la codifica dei file (fine linea da \n a \r\n);

---
+++ tools/n2rn.m
@@ -0,0 +1,56 @@
+function n2rn(path)
+
+%addpath('/var/www/htdocs/work/stralaceno2/tools/')
+
+
+if ~exist(path,'dir')
+    dofile(path)
+    disp('Fatto!')
+    return
+else
+    path
+end
+
+oldpath=cd(path);
+
+z=dir('.');
+for i=1:length(z)
+
+    name = z(i).name;
+    
+    if isdir(name)
+        if ( (name(1) ~= '.') & (~strcmp('CVS',upper(name))) )
+            n2rn([path '/' name]);
+        end
+    else
+        if strcmp(name((end-3):end),'.php')
+            dofile(name)
+        end
+    end
+
+end
+
+cd(oldpath);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function dofile(name)
+
+name2 = [name '.bak'];
+
+disp(['  ' name])
+
+fid=fopen(name);
+fid2=fopen(name2,'w');
+while ~feof(fid)
+    ks=fgetl(fid);
+    %disp(ks)
+    fwrite(fid2,[ks char([13 10])]);
+end
+
+fclose(fid);
+fclose(fid2);
+
+delete(name);
+copyfile(name2,name);
+delete(name2);
