46b304ef06fc1836e9eebde875b70e5afb7c1374
ceres <ceres>
2006-04-13 15:12:34 +0000

    - risposte esatte;

---
+++ custom/lotterie/lotteria_002_ans.php
@@ -0,0 +1,39 @@
+#
+# Una riga per ogni risposta. La 1a riga contiene la risposta esatta alla prima domanda, ecc.
+# Il numero di righe non commentate deve coincidere con le domande della lotteria. 
+# Se una partita non e' stata ancora disputata, inserire la risposta convenzionale "dummy".
+#
+Germania
+Polonia
+Inghilterra
+Svezia
+Argentina
+Olanda
+Portogallo
+Messico
+Italia
+Repubblica Ceca
+Brasile
+Australia
+Francia
+Svizzera
+Spagna
+Ucraina
+Germania
+Argentina
+Italia
+Francia
+Inghilterra
+Olanda
+Brasile
+Spagna
+Germania
+Italia
+Inghilterra
+Brasile
+Germania
+#dummy
+Brasile
+#dummy
+Brasile
+#dummy
