58fedfc57dbfd0c2a00ad169847a49e4e40b9d54
ceres <ceres>
2006-04-14 17:51:23 +0000

    - aggiunti utenti soci ARS (gruppo soci_ars);

--- custom/config/users.php
+++ custom/config/users.php
@@ -10,4 +10,9 @@ ceres::f505a4f2dfe4f6071f265d170c34d2f7::admin,users
 alex::534b44a19bf18d20b71ecc4eb77c572f::admin,users
 nick::e2e42a07550863f8b67f5eb252581f6d::admin,users
 michele::8a94bdfc825df46f880854f41fee346b::users
-
+donatello::a264026f6696433bcdc817e6ac00444b::users
+valentino::3b1b7b5b068e4b863ef29b536c9c2e03::users
+salvatore::7466b376a24aa3d2a44bd303047f7d9e::users
+giuseppe::a04093449efb3aaa937c0d881e74edb5::users
+lello::5c03e87c4e938e3de673ecba916ec7a6::users
+paolo::491c7df7124c75d09dbd906d1cc74aaf::users
