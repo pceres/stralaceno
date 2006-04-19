5c874a15282d2233ab08f08d9efdb6e2a8702414
ceres <ceres>
2006-04-19 20:13:19 +0000

    - i soci ARS appartengono anche al gruppo soci_ars;

--- custom/config/users.php
+++ custom/config/users.php
@@ -6,13 +6,13 @@
 #
 
 [users]
-ceres::f505a4f2dfe4f6071f265d170c34d2f7::admin,users
-alex::534b44a19bf18d20b71ecc4eb77c572f::admin,users
-nick::e2e42a07550863f8b67f5eb252581f6d::admin,users
-michele::8a94bdfc825df46f880854f41fee346b::users
-donatello::a264026f6696433bcdc817e6ac00444b::users
-valentino::3b1b7b5b068e4b863ef29b536c9c2e03::users
-salvatore::7466b376a24aa3d2a44bd303047f7d9e::users
-giuseppe::a04093449efb3aaa937c0d881e74edb5::users
-lello::5c03e87c4e938e3de673ecba916ec7a6::users
-paolo::491c7df7124c75d09dbd906d1cc74aaf::users
+ceres::f505a4f2dfe4f6071f265d170c34d2f7::admin,users,soci_ars
+alex::534b44a19bf18d20b71ecc4eb77c572f::admin,users,soci_ars
+nick::e2e42a07550863f8b67f5eb252581f6d::admin,users,soci_ars
+michele::8a94bdfc825df46f880854f41fee346b::users,soci_ars
+donatello::a264026f6696433bcdc817e6ac00444b::users,soci_ars
+valentino::3b1b7b5b068e4b863ef29b536c9c2e03::users,soci_ars
+salvatore::7466b376a24aa3d2a44bd303047f7d9e::users,soci_ars
+giuseppe::a04093449efb3aaa937c0d881e74edb5::users,soci_ars
+lello::5c03e87c4e938e3de673ecba916ec7a6::users,soci_ars
+paolo::491c7df7124c75d09dbd906d1cc74aaf::users,soci_ars
