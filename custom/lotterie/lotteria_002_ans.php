47d400ac4823eb18d4c9fdafbe0e6ee168d95283
ceres <ceres>
2006-05-03 23:46:48 +0000

    - lotterie dal ramo HEAD;

---
+++ custom/lotterie/lotteria_002_ans.php
@@ -0,0 +1,37 @@
+#
+# Una riga per ogni risposta. La 1a riga contiene la risposta esatta alla prima domanda, ecc.
+# Il numero di righe non commentate deve coincidere con le domande della lotteria. 
+# Se una partita non e' stata ancora disputata, inserire la risposta convenzionale "dummy".
+#
+Brasile
+Francia
+Germania
+Spagna
+Inghilterra
+Italia
+Olanda
+Croazia
+Stati Uniti
+Costa D'Avorio
+Ghana
+Repubblica Ceca
+Italia
+Australia
+Giappone
+Brasile
+Stati Uniti
+Costa D'Avorio
+Ghana
+Giappone
+Stati Uniti
+Ghana
+Stati Uniti
+dummy
+dummy
+dummy
+dummy
+dummy
+dummy
+dummy
+dummy
+
