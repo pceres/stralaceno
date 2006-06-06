0fd6896a35541d7cb74c8247235859f6dba92e91
ceres <ceres>
2006-06-06 22:47:42 +0000

    - file di configurazione dei moduli custom;

---
+++ custom/config/modules_config.php
@@ -0,0 +1,12 @@
+# File di configurazione dei moduli:
+#
+#
+# formato [elenco_moduli]:  nome::descrizione
+#
+#	nome: 		nome del modulo, lo stesso usato nella configurazione del layout colonna sinistra o destra
+#	descrizione:	descrizione associata al modulo
+#
+
+[elenco_moduli]
+lettere_sito::Lettere alla Stralaceno
+pregfas::Pubblico REgistro dei Grandi FAnfaroni della Stralaceno
