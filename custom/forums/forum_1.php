432018733d52f3e618cb7f53bd047921506a3517
ceres <ceres>
2006-07-10 21:18:12 +0000

    - forum privato ARS;

---
+++ custom/forums/forum_1.php
@@ -0,0 +1,18 @@
+[help_config]
+Formato [elenco_topics]:
+<topic_id>::<topic_caption>::<topic_status>::<topic_read_groups>::<topic_write_groups>::<topic_author>::<topic_posts>::<topic_last_post>
+ 	<topic_id>		: numero incrementale che identifica il topic all'interno del forum
+	<topic_caption>		: titolo del topic
+	<topic_status>		: stato del topic:
+				  open 		: il topic e' aperto, si puo' modificare
+				  closed	: il topic e' chiuso, e' in sola lettura
+				  hidden	: il topic e' invisibile/censurato
+	<topic_read_groups>	: gruppi abilitati a leggere il topic
+	<topic_write_groups>	: gruppi abilitati a postare nel topic
+	<topic_author>		: autore del topic (login name)
+	<topic_posts>		: numero di posts nel topic
+	<topic_last_post>	: id dell'ultimo post
+
+[elenco_topics]
+0::Programma corse estate 2006::open::::users::ceres::1::0
+
