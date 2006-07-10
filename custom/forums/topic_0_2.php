ff1ba4066277564a0dd61fba914cbf79e9afc382
ceres <ceres>
2006-07-10 21:18:30 +0000

    - discussioni su forum pubblico;

---
+++ custom/forums/topic_0_2.php
@@ -0,0 +1,12 @@
+[elenco_posts]
+0::Pasquale Ceres::07/07/2006 23.09.32::visible
+
+[post_text_0]
+&nbsp;
+Complimenti a Rocco Del Malandrino e a Tonino Acone: siete stati gli unici due ad indovinare entrambe le squadre finaliste. 
+Spero che abbiate indovinato anche la squadra campione del mondo!
+&nbsp;
+Voi che dite, domani sera vincera' l'Italia? E di quanto?
+&nbsp;
+&nbsp;
+P.S.: per scrivere un nuovo messaggio, premi sul tasto "Rispondi"
