d6a4328a2d0d518afc7b72d958c0395ae423291c
ceres <ceres>
2006-08-04 12:10:13 +0000

    - topic sull'estate caposelese;

---
+++ custom/forums/topic_0_3.php
@@ -0,0 +1,20 @@
+[help_config]
+Formato [elenco_posts]:
+	<post_id>::<post_author>::<post_date>::<post_status>
+ 	<post_id>	: numero incrementale che identifica il post all'interno del topic
+	<post_caption>	: titolo del post
+	<post_status>	: stato del post:
+			  visible	: il post e' visibile
+			  censored	: il topic e' censurato
+			  hidden	: il topic e' invisibile
+Formato [post_text_X]:
+E' il testo del post, puo' essere su piu' righe, nessuna delle quali vuote
+
+[elenco_posts]
+0::Pasquale Ceres::21/07/2006 18.14.21::visible
+
+[post_text_0]
+&nbsp;
+Qualcuno sa dove trovare il programma dell\'estate caposelese 2006?
+&nbsp;
+
