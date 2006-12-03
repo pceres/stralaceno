c54f57de5548aca57469a506d68f365f818d16cf
ceres <ceres>
2006-12-03 12:54:42 +0000

    - aggiornamenti su preparazione sondaggio Champions 2006/07;

--- custom/forums/topic_0_4.php
+++ custom/forums/topic_0_4.php
@@ -13,6 +13,8 @@ E' il testo del post, puo' essere su piu' righe, nessuna delle quali vuote
 [elenco_posts]
 0::Pasquale C.::10/10/2006 22.43.58::visible
 1::Pasquale C.::23/10/2006 23.03.53::visible
+2::Ars::01/12/2006 12.30.55::visible
+3::Pasquale C.::02/12/2006 21.16.33::visible
 
 [post_text_0]
 E' in preparazione un sondaggio sulle squadre prime classificate nella Champions League 2006/2007.
@@ -26,5 +28,14 @@ Se avete suggerimenti sulle modalita' del sondaggio, scriveteci su questo forum,
 [post_text_1]
 E' stata aggiornata la bozza di sondaggio, l'indirizzo e' rimasto lo stesso:
 http://ars.altervista.org/custom/download/Tabellone%20Champions.xls
+&nbsp;
+
+[post_text_2]
+Il sondaggio champions league è agli ultimi giorni di preparazione: sono graditi suggerimenti ed osservazioni. Forza!
+
+[post_text_3]
+Se volete dare uno sguardo alla pagina di inserimento della giocata, attualmente in preparazione, seguite <a href="questions.php?id_questions=2&amp;action=fill&amp;mode=info"> questo link</a>.
+&nbsp;
+I vostri suggerimenti saranno i benvenuti.
 &nbsp;
 
