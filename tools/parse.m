01c760d3cbc0c220e3ce1b97c50c182e72ff0ae9
ceres <ceres>
2006-12-20 23:58:40 +0000

    - il file logfile.txt viene creato automaticamente a partire dai files backupfile_xxx.txt; - non viene rielaborato tutto il file logfile.txt, ma solo l'ultima parte aggiunta;

--- tools/parse.m
+++ tools/parse.m
@@ -21,12 +21,31 @@ if length(backupfile)
     z0 = dir(backupfile);
     somma=0;
     for i=1:length(z0)
-	disp(sprintf('%15s) %d',z0(i).name,z0(i).bytes));
+        disp(sprintf('%15s) %d',z0(i).name,z0(i).bytes));
         somma=somma+z0(i).bytes;
     end
 
     if (somma ~= z.bytes)
-         error(sprintf('Il file di log %s non e'' aggiornato (%d bytes invece di %d)!',nomefile,z.bytes,somma))
+        disp(sprintf('Il file di log %s non e'' aggiornato (%d bytes invece di %d). Lo ricostruisco...',nomefile,z.bytes,somma))
+
+        bulk='';ancora=1;i=0;
+        while ancora,name=sprintf('backupfile%03d.txt',i);
+                if exist(name,'file')
+                        fid=fopen(name);
+                        a=char(fread(fid,'char')');
+                        bulk=[bulk a];
+                        fclose(fid);i=i+1;
+                else
+                        ancora=0;
+                end
+        end
+        fid=fopen('logfile.txt','w');
+        fwrite(fid,bulk);
+        fclose(fid);
+
+        disp(['    Fatto: ' num2str(length(bulk)) ' bytes.'])
+        disp(' ')
+
     end
 end
 
@@ -50,7 +69,25 @@ if must_read
 
     fid = fopen(nomefile);
 
-    bulk = {};
+
+    % leggi da logfile.txt tutte le righe gia' presenti in bulk (letto da logfile.txt)
+    if exist('bulk')
+        count=0;
+        while (count<length(bulk)),
+            count = count+1;
+            fgetl(fid);
+        end
+
+        disp(['Comincio a leggere ' nomefile ' scartando le prime ' num2str(count) ' righe (gia'' presenti in bulk).'])
+        disp(' ')
+
+    else
+        disp(['Devo leggere ' nomefile ' da zero.'])
+        disp(' ')
+
+        bulk = {};
+    end
+
     vmax=zeros(1,7); % sette campi per linea
     while 1
         tline = fgetl(fid);
@@ -68,18 +105,18 @@ if must_read
         for i =1:length(ind2)
             vks{i} = tline(p:(ind2(i)-1));
             p=(ind2(i)+2);
-            
+
             if isempty(vmax)
                 vmax=zeros(1,length(ind2)+1);
             end
             if (vmax(i) < length(vks{i}))
                 vmax(i) = length(vks{i});
             end
-            
+
         end
         if (p <=length(tline))
             vks{i+1} = tline(p:end);
-            
+
             if (vmax(i+1) < length(vks{i+1}))
                 vmax(i+1) = length(vks{i+1});
             end
@@ -88,12 +125,12 @@ if must_read
         %
         % correzioni per riempire eventuali campi assenti
         %
-        
+
         % username mancante
         if (length(vks)==6) % la linea e' stata scritta quando non veniva ancora loggato lo username
             vks{7} = '-';
         end
-        
+
         % nome foto mancante
         if (abilita_stima_foto_mancante & strcmp(vks{1},'foto') & all(vks{2}~='(')) % manca il nome della foto, prova ad aggiungerlo
             ks = vks{2};
@@ -102,7 +139,7 @@ if must_read
             ind2=find(ks == '=');
             id_photo_x=str2num(ks(ind2(1)+1:ind1-1));
             album_x=ks(ind2(2)+1:end);
-            
+
             % individua il tempo numerico
             if isempty(str2num(vks{6}))
                 a2 = vks{6};ind=find(a2==' ');a2 = a2(ind(1)+1:end);a2 = strrep(a2,'st','');a2 = strrep(a2,'nd','');a2 = strrep(a2,'rd','');a2 = strrep(a2,'th','');a2=strrep(a2,' of','');
@@ -111,7 +148,7 @@ if must_read
                 tempo = str2num(vks{6})/(60*60*24)+datenum('1 January 1970 12:00:00 AM');
             end
 
-            
+
             [nome dati]=get_photo_name(tempo,album_x,id_photo_x);
 
             if ~isempty(nome) % se e' riuscito ad individuare la foto, aggiungila in vks{2}
@@ -126,27 +163,52 @@ if must_read
 
     fclose(fid);
 
+
+
     disp(['Ho letto il file ' nomefile '. Ora lo elaboro:'])
 
+    if exist('ip')
+        start = size(ip,1);
+    else
+        start = 0;
+
+        % inizializza extract data
+        label = [];     % tag della pagina visitata
+        arguments = []; % argomenti passati alla pagina
+        ip = [];        % ip da cui proveniva la richiesta
+        referrer = [];  % indirizzo della pagina da cui proveniva la richiesta
+        agent = [];     % browser utilizzato
+        seconds = [];   % istante della richiesta
+        username = [];  % utente logged in
+        vseconds=[];
+    end
+
+    delta = length(bulk)-start;	% numero di campi vuoti (da elaborare) da aggiungere
+
     % extract data
-    label = char(ones(length(bulk),vmax(1))*' ');     % tag della pagina visitata
-    arguments = char(ones(length(bulk),vmax(2))*' '); % argomenti passati alla pagina
-    ip = char(ones(length(bulk),vmax(3))*' ');        % ip da cui proveniva la richiesta
-    referrer = char(ones(length(bulk),vmax(4))*' ');  % indirizzo della pagina da cui proveniva la richiesta
-    agent = char(ones(length(bulk),vmax(5))*' ');     % browser utilizzato
-    seconds = char(ones(length(bulk),vmax(6))*' ');   % istante della richiesta
-    username = char(ones(length(bulk),vmax(7))*' ');  % utente logged in
-    vseconds=[];
-    
+    label     = strvcat(label, char(ones(delta,vmax(1))*' '));     % tag della pagina visitata
+    arguments = strvcat(arguments, char(ones(delta,vmax(2))*' ')); % argomenti passati alla pagina
+    ip        = strvcat(ip, char(ones(delta,vmax(3))*' '));        % ip da cui proveniva la richiesta
+    referrer  = strvcat(referrer, char(ones(delta,vmax(4))*' '));  % indirizzo della pagina da cui proveniva la richiesta
+    agent     = strvcat(agent, char(ones(delta,vmax(5))*' '));     % browser utilizzato
+    seconds   = strvcat(seconds, char(ones(delta,vmax(6))*' '));   % istante della richiesta
+    username  = strvcat(username, char(ones(delta,vmax(7))*' '));  % utente logged in
+    vseconds  = [vseconds];
+
+    start = start+1;
+    disp(['    ...partendo dalla riga ' num2str(start)])
+
+
+
     line_errors = 0;
-    for i =1:length(bulk)
+    for i =start:length(bulk)
 
         vks = bulk{i};
         if length(vks) == 7 % il formato gestito contiene 7 campi
-            
+
             % label
             label(i,1:length(vks{1})) = vks{1};
-            
+
             % argomenti
             if (~isempty(vks{2}))
                 arguments(i,1:length(vks{2})) = vks{2};
@@ -171,30 +233,30 @@ if must_read
                 a2 = strrep(a2,'rd','');
                 a2 = strrep(a2,'th','');
                 a2=strrep(a2,' of','');
-                
+
                 tempo=datenum(a2)+(upper(a2(end-1))=='P')*0.5;
             else
                 tempo = str2num(vks{6})/(60*60*24)+datenum('1 January 1970 12:00:00 AM');
             end
-            vseconds = [vseconds;tempo];
+            vseconds(i) = tempo;
 
             % seconds
             seconds(i,1:length(vks{6})) = vks{6};
 
             % username
             username(i,1:length(vks{7})) = vks{7};
-            
+
         else
             disp(['riga ' num2str(i) ' : '])
             vks{:}
             line_errors = line_errors+1
         end
-        
-        
+
+
         if (mod(i,500)==0)
             disp(['   ' vks{6}]) % ogni tanto visualizza il tempo della linea sotto elaborazione
         end
-        
+
     end
 
     bytes_read = z.bytes;
@@ -231,7 +293,7 @@ for data_i = 1:length(data_list)
         otherwise
             error(['Data_list sconosciuto: ' data_list{data_i}])
     end
-    
+
     v=[];
     list_data='';
     while (size(data,1)>0)
@@ -241,7 +303,7 @@ for data_i = 1:length(data_list)
                 v_(temp_i)=strcmp(data(temp_i,:),data(1,:));
             end
         else
-%             zz = ((data==repmat(data(1,:),size(data,1),1))')
+
             zz=((data==repmat(data(1,:),size(data,1),1))');
             if (size(zz,1) == 1)
                 zz=[zz;zeros(1,size(zz,2))];
@@ -253,19 +315,26 @@ for data_i = 1:length(data_list)
         data=data(find(~v_),:);
     end
     [temp j]=sort(-v);
-    
+
     disp(' ')
     disp(' ')
     disp([data_list{data_i} ' :'])
     disp(' ')
     disp([num2str(v(j)','%5d : '),list_data(j,:)])
-    
-    %
-    % filtri sulle classifiche:
-    %
-    
-    % classifica sulle sole foto (di cui si sa il file)
-    if (strcmp(data_list{data_i},'label_arguments'))
+
+
+
+
+    switch data_list{data_i}
+    case 'label_arguments'
+
+        %
+        % filtri sulle classifiche:
+        %
+        filtro_album = '';	% nome dell'album di cui si vuole avere la classifica
+
+        % classifica sulle sole foto (di cui si sa il file)
+%       if (strcmp(data_list{data_i},'label_arguments'))
         disp(' ')
         disp('Classifica foto piu'' viste: ')
         classifica_foto={};
@@ -279,11 +348,13 @@ for data_i = 1:length(data_list)
                 album=dato((ind3+7):(ind1-1));
                 foto=dato((ind1+1):(ind2-1));
                 disp([num2str(num) ': album ' album ', foto ' foto])
-                
-                classifica_foto{end+1} = {num,album,foto};
+
+                if (isempty(filtro_album) | strcmp(album,filtro_album))
+                    classifica_foto{end+1} = {num,album,foto};
+                end
             end
         end
-        
+
         % crea la figura con tutte le foto (se root_path punta alla radice del sito)
         if (~isempty(root_path) & ~isempty(classifica_foto))
             h=figure;
@@ -300,22 +371,22 @@ for data_i = 1:length(data_list)
                         albumfoto=classifica_foto{somma}{2};
                         nomefoto=classifica_foto{somma}{3};
 
-			matr = [];
+                        matr = [];
                         fotoname=[root_path 'custom/album/' albumfoto '/' nomefoto];
-			if exist(fotoname,'file')
-                        	matr=imread(fotoname);
-			else
-	                        fotoname2=[deleted_photos_path albumfoto '/' nomefoto];
-				if exist(fotoname2,'file')
-        	                	matr=imread(fotoname2);
-					disp(['La foto non esiste piu'', la prendo da ' fotoname2])
-				else
-					disp([fotoname])
-				end
-			end
-			if (~isempty(matr))
-				image(matr);
-			end
+                        if exist(fotoname,'file')
+                                matr=imread(fotoname);
+                        else
+                                fotoname2=[deleted_photos_path albumfoto '/' nomefoto];
+                                if exist(fotoname2,'file')
+                                        matr=imread(fotoname2);
+                                        disp(['La foto non esiste piu'', la prendo da ' fotoname2])
+                                else
+                                        disp([fotoname])
+                                end
+                        end
+                        if (~isempty(matr))
+                                image(matr);
+                        end
                         title([albumfoto ' - ' nomefoto ' : ' num2str(conteggiofoto)],'interpreter','none')
                         axis off;
                         axis image;
@@ -324,18 +395,29 @@ for data_i = 1:length(data_list)
                 end
             end
         end
-        
-    end % fine classifica foto
+
+    %end % fine classifica foto
+
+
+
+    % classifica sistemi operativi
+    case 'agent'
+
+        zz=[num2str(v',3) ones(length(v),1)*' ' list_data];
+
+        ago = 'Linux'
+        for i = 1:size(zz,1),if ~isempty(findstr(ago,zz(i,:))),disp(zz(i,:)),end,end
+
+        ago = 'Windows NT'
+        for i = 1:size(zz,1),if ~isempty(findstr(ago,zz(i,:))),disp(zz(i,:)),end,end
+
+
+
+    end % switch data_list{data_i}
+
+
 
 end
-
-zz=[num2str(v',3) ones(length(v),1)*' ' list_data];
-
-ago = 'Linux'
-for i = 1:size(zz,1),if ~isempty(findstr(ago,zz(i,:))),disp(zz(i,:)),end,end
-
-ago = 'NT4'
-for i = 1:size(zz,1),if ~isempty(findstr(ago,zz(i,:))),disp(zz(i,:)),end,end
 
 
 % grafico accessi pagine
@@ -343,7 +425,9 @@ figure,subplot(1,1,1),hold off
 hist(fix(vseconds),round(vseconds(end)-vseconds(1)));
 vett_num = hist(fix(vseconds),round(vseconds(end)-vseconds(1)));zz=datestr(unique(fix(vseconds)));ind=round(1:((size(zz,1)-1)/6):size(zz,1));set(gca,'xticklabel',zz(ind,:),'xtick',datenum(zz(ind,:)),'xgrid','on')
 vett_x = unique(fix(vseconds))-1;
-hold on;plot([1 1]*datenum('2 september 2005'),get(gca,'Ylim'),'r')
+% righe segnadata verticali:
+hold on;plot([1 1]*datenum('2 september 2005'),get(gca,'Ylim'),'r') % edizione 2005
+hold on;plot([1 1]*datenum('30 august 2006'),get(gca,'Ylim'),'r')   % edizione 2006
 
 [y,m] = datevec(min(vseconds));
 [y2,m2] = datevec(max(vseconds));
@@ -364,3 +448,21 @@ tag='homepage'
 w=zeros(7,1);zz=datestr(vseconds,'ddd');zzu=unique(zz,'rows');zzu=zzu([2 6 7 5 1 3 4],:);for i=1:size(zzu,1),for ii = 1:size(zz,1);if (strcmp(zz(ii,:),zzu(i,:)) & strfind(label(ii,:),tag)),w(i)=w(i)+1;end,end,end
 [zzu repmat(' ',length(w),1) num2str(w)]
 figure,bar(w);set(gca,'XTickLabel',zzu);title(tag)
+
+
+
+
+%
+% individua le pagine in uscita
+%
+
+% ip=ip(1:10,:);
+% label=label(1:10,:);
+% vseconds=vseconds(1:10);
+
+if (0)
+	disp('individua le pagine in uscita...')
+	pause
+	
+	parse3
+end
