338995cffa95d1365e04c2e2f344ea126bb20576
ceres <ceres>
2007-01-07 22:10:19 +0000

    - aggiunta password per sezione "FC_caposele" (palumenta);

--- admin/manage_articles.php
+++ admin/manage_articles.php
@@ -65,6 +65,9 @@ case 'homepage':
 case 'ciclismo':
 	$password_ok = 'f055d8b5317237d7e3e50b3c3c38667c'; // "Bartali"
 	break;
+case 'FC_caposele':
+	$password_ok = 'd5aa82c231314da451812262871076bf'; // "palumenta"
+	break;
 }
 
 if ($password != $password_ok)
