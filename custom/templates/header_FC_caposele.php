b5738b78cc5d4d1126ca7716c1bb372c8b390905
ceres <ceres>
2007-01-14 19:58:43 +0000

    - header personalizzato per sezione FC_caposele;

---
+++ custom/templates/header_FC_caposele.php
@@ -0,0 +1,16 @@
+<!-- 
+inizio header
+-->
+	<table align="center">
+	  <tbody>
+	  
+		<tr>
+		  <td> <img src="<?php echo $site_abs_path?>custom/images/logo.gif" alt="logo_ars"> </td>
+		  <td> sezione <big><b>F.C. Caposele</b></big> </td>
+		</tr>
+		
+	  </tbody>
+	</table>
+<!-- 
+fine header
+-->
