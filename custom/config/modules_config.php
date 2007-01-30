1aaef84d66e442bf841976e061589687a588e877
ceres <ceres>
2007-01-30 23:16:26 +0000

    - aggiunti nuovi moduli;

--- custom/config/modules_config.php
+++ custom/config/modules_config.php
@@ -11,3 +11,7 @@
 lettere_sito::Lettere alla Stralaceno
 pregfas::Pubblico REgistro dei Grandi FAnfaroni della Stralaceno
 soci::Elenco dei soci e news ARS Amatori Running Sele
+flash_news::News flash
+siti_amici::Elenco siti caposelesi (Caposele sul Web)
+archivio::Archivio album ed articoli
+classifica_campionato::Classifica e risultati FC Caposele
