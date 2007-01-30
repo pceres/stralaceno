3bb491544a770fb1ef55e3b5d070e32b09470c1a
ceres <ceres>
2007-01-30 23:17:38 +0000

    - utenti aggiornati dal sito ArsWeb;

--- custom/config/users.php
+++ custom/config/users.php
@@ -6,10 +6,11 @@
 #
 
 [users]
+# soci ARS:
 ceres::f505a4f2dfe4f6071f265d170c34d2f7::admin,users,soci_ars,root_admin
-alex::534b44a19bf18d20b71ecc4eb77c572f::admin,users,soci_ars
+alex::0024f84a5f3de0025c45b906a522e943::admin,users,soci_ars
 nick::e2e42a07550863f8b67f5eb252581f6d::admin,users,soci_ars
-michele::8a94bdfc825df46f880854f41fee346b::users,soci_ars
+michele::8a94bdfc825df46f880854f41fee346b::admin,users,soci_ars
 donatello::a264026f6696433bcdc817e6ac00444b::users,soci_ars
 valentino::3b1b7b5b068e4b863ef29b536c9c2e03::users,soci_ars
 salvatore::7466b376a24aa3d2a44bd303047f7d9e::users,soci_ars
@@ -19,3 +20,8 @@ paolo::491c7df7124c75d09dbd906d1cc74aaf::users,soci_ars
 martino::5a682d4fe8da9adc95a383742ea30c59::users,soci_ars
 nicola.cirillo::0d4f9d4cb7e6ed8ecbaad6fd16117581::users,soci_ars
 rocco.proietto::7e3217beee39079a345d9b600ebd3b84::users,soci_ars
+francesco.ceres::23c47776c97d717cbb5809ccf1d34fab::users,soci_ars
+gianni.sozio::7e4d797ce474328ed691f97415ad81f9::users,soci_ars
+salvatore.colatrella::4c64ba31dd74feaa56986f08e09b1768::users,soci_ars
+# non soci ARS:
+don.ger::1e8c22feeac792722fb8738f702b9ef5::admin,users
