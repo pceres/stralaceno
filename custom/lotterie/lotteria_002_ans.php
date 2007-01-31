b8ab7a0f2bacb0e511fdfee9a9323014d1c24b60
ceres <ceres>
2007-01-31 22:29:43 +0000

    - la lotteria 2 e' quella sulla Champions League 2006/07;

--- custom/lotterie/lotteria_002_ans.php
+++ custom/lotterie/lotteria_002_ans.php
@@ -1,16 +1,74 @@
 #
 # Una riga per ogni risposta. La 1a riga contiene la risposta esatta alla prima domanda, ecc.
-# Il numero di righe non commentate deve coincidere con le domande della lotteria. 
-# Se una partita non e' stata ancora disputata, inserire la risposta convenzionale "dummy".
+# Il numero di righe non commentate deve coincidere con le domande della lotteria.
+# Se una partita non e' stata ancora disputata, inserire la risposta convenzionale "nd".
 #
-dummy nominativo
-Gelsomino Merola
-Francesco Ceres di Gius.
-Paolo Viscardi
-Gerardo Martino
-Salvatore Merola
-Rocco Feniello
-Francesco Ciccone
-Alessandro Russomanno
-Alessio Zarra
-Claudio Donatiello
+
+# Qualificato quarti 1:
+nd
+#Barcellona
+# Qualificato quarti 2:
+nd
+#Bayern Monaco
+# Qualificato quarti 3:
+nd
+#Galatasaray
+# Qualificato quarti 4:
+nd
+#Olympiakos Pireo
+# Qualificato quarti 5:
+nd
+#Dynamo Kiev
+# Qualificato quarti 6:
+nd
+#Fc Copenhagen
+# Qualificato quarti 7:
+nd
+#Amburgo
+# Qualificato quarti 8:
+nd
+#Anderlecht
+# Qualificato semifinale 1:
+nd
+#Barcellona
+# Qualificato semifinale 2:
+nd
+#Bayern Monaco
+# Qualificato semifinale 3:
+nd
+#Galatasaray
+# Qualificato semifinale 4:
+nd
+#Olympiakos Pireo
+# Qualificato finale 1:
+nd
+#Barcellona
+# Qualificato finale 2:
+nd
+#Bayern Monaco
+# Vincitore:
+nd
+#Barcellona
+
+#
+# Le risposte sono suddivise in gruppi. Per ciascun gruppo esiste un set di risposte piu' o meno corrette
+#
+[equivalenza_risposte]
+4::qualif. quarti	::0,1,2,3,4,5,6,7
+3::qualif. semifinali	::8,9,10,11
+2::qualif. finali	::12,13
+1::vincitore		::14
+
+#
+# Qui viene indicato il punteggio da associare ad ogni risposta a seconda del gruppo in cui essa viene fornita
+#
+[punteggio_risposte]
+#
+#Barcellona::4,4;3,4;2,4;1,2
+#Bayern Monaco::4,4;3,4;2,4;1,2
+#Galatasaray::4,4;3,4
+#Olympiakos Pireo::4,4;3,3
+#Dynamo Kiev::4,4;3,1
+#FC Copenhagen::4,4
+#Amburgo::4,4
+#Anderlecht::4,4
