91d5023fa5336a163cdc1528009a8e9ceece3473
ceres <ceres>
2007-02-08 23:46:20 +0000

    - topic su FC Caposele;

---
+++ custom/forums/topic_0_14.php
@@ -0,0 +1,18 @@
+[help_config]
+Formato [elenco_posts]:
+	<post_id>::<post_author>::<post_date>::<post_status>
+ 	<post_id>	: numero incrementale che identifica il post all'interno del topic
+	<post_caption>	: titolo del post
+	<post_status>	: stato del post:
+			  visible	: il post e' visibile
+			  censored	: il topic e' censurato
+			  hidden	: il topic e' invisibile
+Formato [post_text_X]:
+E' il testo del post, puo' essere su piu' righe, nessuna delle quali vuote
+
+[elenco_posts]
+0::Ars::01/02/2007 16.38.00::visible
+
+[post_text_0]
+Qui aspettiamo i vostri commenti alle vicende calcistiche della F.C. Caposele.
+
