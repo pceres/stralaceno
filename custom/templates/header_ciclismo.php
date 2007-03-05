fb3d42698c4f690e2881b96ad55bbb7ab24c7524
ceres <ceres>
2007-03-05 23:05:19 +0000

    - header sezione "ciclismo";

---
+++ custom/templates/header_ciclismo.php
@@ -0,0 +1,16 @@
+<!-- 
+inizio header
+-->
+	<table align="center">
+	  <tbody>
+	  
+		<tr>
+		  <td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo.jpg" alt="logo %web_title%"> </td>
+		  <td> sezione <big><big><b>Ciclismo</b></big></big> </td>
+		</tr>
+		
+	  </tbody>
+	</table>
+<!-- 
+fine header
+-->
