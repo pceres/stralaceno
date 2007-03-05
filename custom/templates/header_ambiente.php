4e50141a34eef1e873ea9c0b1b72f15a057172db
ceres <ceres>
2007-03-05 23:05:31 +0000

    - header sezione "ambiente";

---
+++ custom/templates/header_ambiente.php
@@ -0,0 +1,17 @@
+<!-- 
+inizio header
+-->
+	<table align="center">
+	  <tbody>
+	  
+		<tr>
+		  <td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo.jpg" alt="logo %web_title%"> </td>
+		  <td> sezione <big><big><b>Ambiente</b></big></big> </td>
+		  <td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo_ambiente-thumb.jpg" alt="logo sezione ambiente" width="120"> </td>
+		</tr>
+		
+	  </tbody>
+	</table>
+<!-- 
+fine header
+-->
