cab12fa34f7cd99776825dc3af9bed5cbe148c36
ceres <ceres>
2007-05-13 14:18:42 +0000

    - aggiunto il link al feed RSS 2.0;

--- index_right.php
+++ index_right.php
@@ -26,10 +26,24 @@ foreach($elenco_layout_right as $riquadro => $list_items)
 
 </tbody></table>
 
-	<div align="right" class="txt_normal"><i>
+	<div align="right" class="txt_normal">
+		<!-- fankounter -->
 		<br>
-		<script language="JavaScript"  type="text/javascript" src="<?php echo $script_abs_path; ?>fkounter5/counter.js.php?id=<?php echo $root_prefix; ?>&amp;mode=graphic"></script>
-	</i></div>
+		<i>
+			<script language="JavaScript"  type="text/javascript" src="<?php echo $script_abs_path; ?>fkounter5/counter.js.php?id=<?php echo $root_prefix; ?>&amp;mode=graphic"></script>
+		</i>
+		
+		<!-- feed RSS -->
+		<br>
+		<br>
+		<span title="Per usare RSS, copiare il link ed inserirlo in un aggregatore RSS, es. NewsFox">
+			Feed RSS 2.0:
+			<a href="feed.php">
+				<img src="<?php echo $site_abs_path; ?>images/syndicated-feed-icon.gif" alt="RSS 2.0 feed icon" style="border: 0px none;">
+			</a>
+		</span>
+
+	</div>
 
 <!-- 
 fine colonna destra
