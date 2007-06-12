a3e21e1c05d132c77773457945768b44ca4faefb
ceres <ceres>
2007-06-12 18:38:33 +0000

    - aggiunta gestione dei dati di autenticazione: vengono visualizzati solo i contenuti consentiti per il gruppo di appartenenza;

--- feed.php
+++ feed.php
@@ -2,9 +2,11 @@
 
 require_once('libreria.php');
 require_once('last_contents_lib.php');	// gestione ultimi contenuti
+require_once('login.php');		// gestione autenticazione
 
-$feed = read_last_contents();	// carica i dati degli ultimi contenuti aggiunti
-publish_rss20($feed);		// genera il testo XML del feed RSS 2.0
+
+$feed = read_last_contents($login);	// carica i dati degli ultimi contenuti aggiunti
+publish_rss20($feed);			// genera il testo XML del feed RSS 2.0
 
 # logga il contatto
 $counter = count_page("feed",array("COUNT"=>1,"LOG"=>1),$filedir_counter); # abilita il contatore, senza visualizzare le cifre, e fai il log
