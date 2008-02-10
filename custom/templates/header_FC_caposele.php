9d3866bcb9239ad42a07c0da018befea863c287b
ceres <ceres>
2008-02-10 16:11:48 +0000

    - nuovo logo FC Caposele;

--- custom/templates/header_FC_caposele.php
+++ custom/templates/header_FC_caposele.php
@@ -5,8 +5,13 @@ inizio header
 	  <tbody>
 	  
 		<tr>
-		  <td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo.jpg" alt="logo %web_title%"> </td>
-		  <td> sezione <big><big><b>F.C. Caposele</b></big></big> </td>
+		  <td align="left"> <img src="<?php echo $site_abs_path?>custom/album/varie/logo.jpg" alt="logo %web_title%" width="100%"> </td>
+		  <td width="40%"><big> sezione <big><big><b>F.C. Caposele</b></big></big></big> </td>
+		  <td align="right">
+			<a href="<?php echo $site_abs_path?>custom/album/FC_caposele/LOGOLIT.jpg">
+			<img src="<?php echo $site_abs_path?>custom/album/FC_caposele/LOGOLIT.jpg" alt="logo F.C. Caposele" width="64%">
+			</a> 
+		  </td>
 		</tr>
 		
 	  </tbody>
