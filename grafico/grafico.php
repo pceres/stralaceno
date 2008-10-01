d5f5132ded774acb8732d3e8ef7b2f3d3e5f9660
ceres <ceres>
2008-10-01 21:29:02 +0000

    - aggiunti header http per impedire il caching dell'immagine;

--- grafico/grafico.php
+++ grafico/grafico.php
@@ -296,7 +296,10 @@ if ($has_legend==1) //disegna legenda?
 	}
 if ($output_to_file==0)
 {
-    header ("Content-type: image/PNG");
+	header ("Content-type: image/PNG");
+	header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+	header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");   // Date in the past
+	
 	ImagePNG($img_handle);
 }
 else
