8f5741ee8e1cf45906c3e8d8c744075483a0ef50
ceres <ceres>
2009-04-23 20:21:51 +0000

    - gestione file di configurazione multipli (per piu' pagine simili);

--- custom/moduli/siti_amici/siti_amici.php
+++ custom/moduli/siti_amici/siti_amici.php
@@ -5,17 +5,18 @@ require_once('../../../libreria.php');
 # dichiara variabili
 extract(indici());
 
-
 ?>
 
 <?php
-// determina il nome del modulo, ed il path assoluto
-$filename = $_SERVER[SCRIPT_FILENAME];						// path assoluto e nome dello script in esecuzione
+$sezione     = sanitize_user_input($_REQUEST['page'],'plain_text',Array());		// determina la sezione
+$module_data = sanitize_user_input($_REQUEST['module_data'],'plain_text',Array());	// personalizzazione file di cfg.
+
+$filename = $_SERVER[SCRIPT_FILENAME];			// path assoluto e nome dello script in esecuzione
 $module_endpath = substr($filename,strpos($filename,'custom/moduli/')+14);	// path della cartella contenente i moduli (custom/moduli/)
 $module_name = substr($module_endpath,0,strrpos($module_endpath,'/'));		// nome del modulo in esecuzione
-$module_abs_path = $modules_dir."$module_name/";				// path completo allo script in esecuzione
+$module_abs_path = $modules_dir."$module_name/";	// path completo allo script in esecuzione
 
-show_template($module_abs_path,$module_name.".tpl");
+show_template($module_abs_path,$module_name.".tpl",$sezione,$module_data);
 
 # logga il contatto
 $counter = count_page("modulo_$module_name",array("COUNT"=>1,"LOG"=>1),$filedir_counter); # abilita il contatore, senza visualizzare le cifre, e fai il log
