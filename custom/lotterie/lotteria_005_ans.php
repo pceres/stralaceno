ca666d16b252baf327f8045adfee1b434f5b2cda
pceres <pasquale_c@hotmail.com>
2011-12-24 19:10:28 +0100

    aggiunta lotteria_05 (Europei 2012)
    new file:   custom/lotterie/lotteria_005.txt
    new file:   custom/lotterie/lotteria_005_ans.php
    new file:   custom/lotterie/lotteria_005_log.txt
    new file:   custom/lotterie/lotteria_005_tpl_form.php
    new file:   custom/lotterie/lotteria_005_tpl_results.php

---
+++ custom/lotterie/lotteria_005_ans.php
@@ -0,0 +1,67 @@
+#
+# Una riga per ogni risposta. La 1a riga contiene la risposta esatta alla prima domanda, ecc.
+# Il numero di righe non commentate deve coincidere con le domande della lotteria.
+# Se una partita non e' stata ancora disputata, inserire la risposta convenzionale "nd".
+#
+
+# Qualificato quarti 1:
+Polonia
+# Qualificato quarti 2:
+Russia
+# Qualificato quarti 3:
+nd
+# Qualificato quarti 4:
+nd
+# Qualificato quarti 5:
+nd
+# Qualificato quarti 6:
+nd
+# Qualificato quarti 7:
+nd
+# Qualificato quarti 8:
+nd
+# Qualificato semifinale 1:
+Polonia
+# Qualificato semifinale 2:
+Russia
+# Qualificato semifinale 3:
+nd
+# Qualificato semifinale 4:
+nd
+# Qualificato finale 1:
+Polonia
+# Qualificato finale 2:
+Russia
+# Vincitore:
+Russia
+
+#
+# Le risposte sono suddivise in gruppi. Per ciascun gruppo esiste un set di risposte piu' o meno corrette
+#
+[equivalenza_risposte]
+4::qualif. quarti	::0,1,2,3,4,5,6,7
+3::qualif. semifinali	::8,9,10,11
+2::qualif. finali	::12,13
+1::vincitore    	::14
+
+#
+# Qui viene indicato il punteggio da associare ad ogni risposta a seconda del gruppo in cui essa viene fornita
+#
+# es: Croazia::4,0;3,4;2,0
+[punteggio_risposte]
+Polonia::4,0;3,0;2,0;1,0
+Grecia::
+Russia::4,4;3,4;2,4;1,4
+Repubblica Ceca::
+Olanda::
+Danimarca::
+Germania::
+Portogallo::
+Spagna::
+Italia::
+Irlanda::
+Croazia::
+Ucraina::
+Svezia::
+Francia::
+Inghilterra::
