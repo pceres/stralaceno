567d51acea077f573aaf3032879c1d76e0067033
pceres <pasquale_c@hotmail.com>
2013-02-03 22:33:40 +0100

    ars_local: sezione Ambiente --> Escursionismo
    modified:   custom/config/layout_right.txt
    modified:   custom/templates/header_ambiente.php

--- custom/templates/header_ambiente.php
+++ custom/templates/header_ambiente.php
@@ -6,8 +6,8 @@ inizio header
 	  
 		<tr>
 		  <td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo.jpg" alt="logo %web_title%"> </td>
-		  <td> sezione <big><big><b>Ambiente</b></big></big> </td>
-		  <td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo_ambiente-thumb.jpg" alt="logo sezione ambiente" width="120"> </td>
+		  <td> sezione <big><big><b>Escursionismo</b></big></big> </td>
+		  <!--<td> <img src="<?php echo $site_abs_path?>custom/album/varie/logo_ambiente-thumb.jpg" alt="logo sezione ambiente" width="120"> </td>  -->
 		</tr>
 		
 	  </tbody>
