2042a7441d0c1dfebef247b798fe83b6cad828ec
pceres <pasquale_c@hotmail.com>
2014-07-05 21:06:35 +0200

    ars: aggiornato al 5 luglio 2014
    modified:   custom/lotterie/lotteria_007.txt
    modified:   custom/lotterie/lotteria_007_ans.php
    modified:   custom/lotterie/lotteria_008.txt
    modified:   custom/lotterie/lotteria_008_ans.php

--- custom/lotterie/lotteria_008_ans.php
+++ custom/lotterie/lotteria_008_ans.php
@@ -39,23 +39,23 @@ Algeria
 # Qualificato quarti 1:
 Brasile
 # Qualificato quarti 2:
-nd
+Colombia
 # Qualificato quarti 3:
-nd
+Olanda
 # Qualificato quarti 4:
-nd
+Costa Rica
 # Qualificato quarti 5:
-nd
+Francia
 # Qualificato quarti 6:
-nd
+Germania
 # Qualificato quarti 7:
-nd
+Argentina
 # Qualificato quarti 8:
-nd
+Belgio
 # Qualificato semifinale 1:
-nd
+Germania
 # Qualificato semifinale 2:
-nd
+Brasile
 # Qualificato semifinale 3:
 nd
 # Qualificato semifinale 4:
@@ -82,36 +82,36 @@ nd
 #
 # es: Brasile::5,0;4,0;3,0;2,0;1,0
 [punteggio_risposte]
-Brasile::5,4;4,2
+Brasile::5,4;4,2;3,8
 Croazia::5,0;4,0;3,0;2,0;1,0
-Messico::5,4
+Messico::5,4;4,0;3,0;2,0;1,0
 Camerun::5,0;4,0;3,0;2,0;1,0
 Spagna::5,0;4,0;3,0;2,0;1,0
-Olanda::5,4
+Olanda::5,4;4,4
 Cile::5,4;4,2;3,0;2,0;1,0
 Australia::5,0;4,0;3,0;2,0;1,0
-Colombia::5,4
-Grecia::5,4
+Colombia::5,4;4,4;3,0;2,0;1,0
+Grecia::5,4;4,2;3,0;2,0;1,0
 Costa D'Avorio::5,0;4,0;3,0;2,0;1,0
 Giappone::5,0;4,0;3,0;2,0;1,0
-Uruguay::5,4
-Costa Rica::5,4
+Uruguay::5,4;4,0;3,0;2,0;1,0
+Costa Rica::5,4;4,2
 Inghilterra::5,0;4,0;3,0;2,0;1,0
 Italia::5,0;4,0;3,0;2,0;1,0
-Svizzera::5,4
+Svizzera::5,4;4,1;3,0;2,0;1,0
 Ecuador::5,0;4,0;3,0;2,0;1,0
-Francia::5,4
+Francia::5,4;4,4;3,0;2,0;1,0
 Honduras::5,0;4,0;3,0;2,0;1,0
-Argentina::5,4
+Argentina::5,4;4,3
 Bosnia-Herzegovina::5,0;4,0;3,0;2,0;1,0
 Iran::5,0;4,0;3,0;2,0;1,0
-Nigeria::5,4
-Germania::5,4
+Nigeria::5,4;4,0;3,0;2,0;1,0
+Germania::5,4;4,3;3,8
 Portogallo::5,1;4,0;3,0;2,0;1,0
 Ghana::5,0;4,0;3,0;2,0;1,0
-USA::5,3
-Belgio::5,4
-Algeria::5,4
+USA::5,3;4,1;3,0;2,0;1,0
+Belgio::5,4;4,3
+Algeria::5,4;4,1;3,0;2,0;1,0
 Russia::5,0;4,0;3,0;2,0;1,0
 Corea Del Sud::5,0;4,0;3,0;2,0;1,0
 
