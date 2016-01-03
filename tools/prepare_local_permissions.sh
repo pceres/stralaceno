82f98da80ffea640b1588a190701c6e683d3b55c
pceres <pasquale_c@hotmail.com>
2016-01-03 15:00:20 +0100

    ars_local: script to force file permissions to work in local
    new file:   tools/prepare_local_permissions.sh

---
+++ tools/prepare_local_permissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+chmod a+w custom/moduli/last_contents/last_contents_cfg.txt
+chmod a+w custom/contatori/log_contents.php
+chmod a+w custom/config/download_cfg.php
+chmod a+w custom/lotterie/lotteria_*_ans.php
+chmod a+w custom/articoli/*.txt
+
+rm custom/config/challenge.php
