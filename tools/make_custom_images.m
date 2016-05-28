e384bf7d0ddf79de61a3dfbceacfce16eaf4b9b3
pceres <pasquale_c@hotmail.com>
2016-05-28 16:57:37 +0200

    stralaceno: automatical download of pgv images in custom album
    modified:   tools/make_custom_images.m

--- tools/make_custom_images.m
+++ tools/make_custom_images.m
@@ -6,76 +6,26 @@ function make_custom_images
 
 atleti_laceno = '../custom/dati/atleti_laceno.csv';
 folder_images = '../custom/album/custom/';
-url_format  = 'http://ars.altervista.org/PhpGedView/treenav.php?ged=caposele&rootid=<PID>';
+pgvroot  = 'http://ars.altervista.org/PhpGedView/';
+pgvroot  = 'http://localhost/work/PhpGedView/'
+gedcom = 'caposele';
 flg_download_all = 1; % flag to download all images, not only the missing ones
 
 [data str] = read_csv(atleti_laceno);
 
 [v_status_img list_images] = analyse_data(data,str,folder_images,flg_download_all);
 
-prepare_images(data,str,list_images,url_format)
+% prepare list of SID (cell array of strings)
+list_SID = data(list_images,9);
 
+% show preliminary message
+disp('Be sure that:')
+disp('- Firefox is open and there are no additional message bars (eg. sync or find)')
+disp(['- you logged into the pgv website ' pgvroot])
+input('Press ENTER to start downloading...')
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function javaimg = prepare_images(data,str,list_images,url_format)
-
-dest_folder = 'snapshot';
-
-robot = robot_wrapper('init');
-
-screenSize = get(0, 'screensize');
-width  = screenSize(3);
-height = screenSize(4);
-
-% give focus to the browser
-robot_wrapper('mouse_move',{robot,width*0.20, height*0.105});
-robot_wrapper('mouse_click',{robot,'left'});
-pause(0.3)
-
-if ~exist(dest_folder,'dir')
-    mkdir(dest_folder)
-end
-
-for i_atl = list_images
-    id       = data{i_atl,str.ind_id};
-    name     = data{i_atl,str.ind_name};
-    % image    = data{i_atl,str.ind_image};
-    id_genea = data{i_atl,str.ind_id_genea};
-    
-    url = strrep(url_format,'<PID>',id_genea);
-    
-    % give focus to the browser url control
-    robot_wrapper('mouse_move',{robot,width*0.20, height*0.105});
-    robot_wrapper('mouse_click',{robot,'left'});
-    pause(0.2)
-    robot_wrapper('key_press',{robot,'^(a)'}); % select all
-    pause(0.2)
-    robot_wrapper('key_press',{robot,url}); % type the url
-    pause(0.2)
-    robot_wrapper('key_press',{robot,sprintf('\n')}); % enter
-    pause(2) % wait for page load
-    
-    % scroll the window
-    robot_wrapper('mouse_move',{robot,width*0.995, height*0.925});
-    for i_tmp = 1:4
-        robot_wrapper('mouse_click',{robot,'left'});
-        pause(0.2)
-    end
-    
-    % move graph a bit to the right
-    robot_wrapper('mouse_move',{robot,width*0.01, height*0.47})
-    pause(0.2)
-    robot_wrapper('mouse_move_with_button_pressed',{robot,'left',width*0.055, height*0.47})
-    
-    
-    %%input('Please position the graph in the middle of the screen, then press ENTER','s')
-    
-    filename = [dest_folder filesep id_genea '.jpg'];
-    javaimg = robot_wrapper('save_snapshot',{robot,filename});
-    
-    fprintf(1,'%3s: %-30s : %6s %s\n',id,name,id_genea,url)
-end
+% launch the downloader
+download_pgv_images(pgvroot,gedcom,list_SID,folder_images)
 
 
 
