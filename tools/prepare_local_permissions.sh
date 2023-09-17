fa7c859092f7958142405a72e2747bff5f40609d
pceres <pasquale_c@hotmail.com>
2023-09-17 18:09:07 +0200

    stralaceno: fix stralaceno files, mng style.css
    nuovo file:             tools/prepare_local_permissions.sh

---
+++ tools/prepare_local_permissions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# fix file permissions
+chmod a+w custom/articoli/*.txt
+#chmod a+w custom/config/download_cfg.php
+chmod a+r custom/config/style.css
+#chmod a+w custom/contatori/log_contents.php
+chmod a+w custom/contatori/lasthitfile.txt
+chmod a+w custom/contatori/logfile.txt
+#chmod a+w custom/contatori/data/data_ars.php
+#chmod a+w custom/contatori/temp/accs_ars.dat
+chmod a+w custom/lotterie/lotteria_*_ans.php
+#chmod a+w custom/moduli/last_contents/last_contents_cfg.txt
+
+# remove temporary files
+rm custom/config/challenge.php
+
+# fix folder permission
+chmod a+w custom/config
+chmod a+w custom/contatori          # backupfile
+chmod a+w custom/contatori/back
+chmod a+w custom/contatori/temp
