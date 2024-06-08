6c5e53d815d63debfbcb69f5aef8d6f703c877b4
pceres <pasquale_c@hotmail.com>
2024-06-08 20:31:58 +0200

    update to 08/06/2024
    modificato:             custom/contatori/counterfile.txt
    modificato:             custom/contatori/data/data_ars.php
    modificato:             custom/lotterie/lotteria_013.txt
    modificato:             custom/lotterie/lotteria_013_ans.php
    modificato:             custom/lotterie/lotteria_013_log.txt
    modificato:             custom/moduli/archivio/archivio_cfg.txt

--- custom/lotterie/lotteria_013_ans.php
+++ custom/lotterie/lotteria_013_ans.php
@@ -5,7 +5,7 @@
 #
 
 # Qualificato ottavi 1:
-Germania
+nd
 # Qualificato ottavi 2:
 nd
 # Qualificato ottavi 3:
@@ -86,8 +86,8 @@ nd
 # Brasile::5,20;4,0             # se il Brasile passa il primo turno ma esce al secondo, e non sono state giocate altre partite
 # Brasile::5,20;4,0;3,0;2,0;1,0 # se il Brasile passa il primo turno ma esce al secondo, e sono state giocate tutte le partite
 [punteggio_risposte]
-Germania::5,20
-Ungheria::5,6
+Germania::
+Ungheria::
 Scozia::
 Svizzera::
 Spagna::
