06ec43495874691a88a71abb2ba471805a89108b
pceres <pasquale_c@hotmail.com>
2026-04-04 18:50:06 +0200

    ars: fix permissions in contatori/temp folder
    modificato:             prepare_local_permissions.sh

--- tools/prepare_local_permissions.sh
+++ tools/prepare_local_permissions.sh
@@ -16,7 +16,7 @@ chmod a+w custom/config/*.txt
 chmod a+w custom/contatori/lasthitfile.txt
 chmod a+w custom/contatori/logfile.txt
 chmod a+w custom/contatori/data/data_ars.php
-chmod a+w custom/contatori/temp/accs_ars.dat
+chmod a+w custom/contatori/temp/ -R
 chmod a+w custom/lotterie/lotteria_*_ans.php
 #chmod a+w custom/moduli/last_contents/last_contents_cfg.txt
 
